2fee4b026884fc994ccad4366f452d8b20449361
Paul Edwards <paul.edwards@microsoft.com>
2020-11-23 11:26:32 +0000

    unique name for shared image gallery

--- tf/sig.tf
+++ tf/sig.tf
@@ -1,5 +1,5 @@
 resource "azurerm_shared_image_gallery" "sig" {
-  name                = "hpcready"
+  name                = "hpcready_${random_string.random.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   description         = "Shared images for HPC"
