417da06a25001929b7b3112dd997b7b8b4ebbafb
Paul Edwards <paul.edwards@microsoft.com>
2020-11-24 11:07:59 +0000

    Adding dns for ondemand

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -6,6 +6,8 @@ resource "local_file" "AnsibleInventory" {
   jumpbox-user = azurerm_linux_virtual_machine.jumpbox.admin_username,
   scheduler-ip = azurerm_network_interface.scheduler-nic.private_ip_address,
   scheduler-user = azurerm_linux_virtual_machine.scheduler.admin_username,
+  ondemand-ip = azurerm_network_interface.ondemand-nic.private_ip_address,
+  ondemand-user = azurerm_linux_virtual_machine.ondemand.admin_username,
   ad-ip = azurerm_network_interface.ad-nic.private_ip_address,
   ad-passwd = azurerm_windows_virtual_machine.ad.admin_password,
  }
