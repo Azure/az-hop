f83c443262d48c4635c2b3c9d38909188729f30b
xpillons <xpillons@microsoft.com>
2020-11-25 15:07:10 +0000

    fix destroy script

--- destroy.sh
+++ destroy.sh
@@ -2,4 +2,4 @@
 
 RESOURCE_GROUP=$(cat resource_group.txt)
 terraform init ./tf
-terraform apply -auto-approve -parallelism=30 -var location=westeurope -var resource_group=$RESOURCE_GROUP ./tf
+terraform destroy -auto-approve -parallelism=30 -var location=westeurope -var resource_group=$RESOURCE_GROUP ./tf
