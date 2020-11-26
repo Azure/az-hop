982f519f455eba2c28a676d41e97669a94c8f058
xpillons <xpillons@microsoft.com>
2020-11-26 22:53:27 +0000

    NSG for frontend subnet

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 pip3 install pypsrp
 pip3 install pysocks
-#ansible-galaxy collection install ansible.windows
-#ansible-galaxy collection install community.windows
+ansible-galaxy collection install ansible.windows
+ansible-galaxy collection install community.windows
+ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install community.general
 
 ansible-playbook -i playbooks/inventory ./playbooks/ad.yml
