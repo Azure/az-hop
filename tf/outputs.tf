46201da3592211d54658f4888bbf3d506950efd5
Paul Edwards <paul.edwards@microsoft.com>
2020-11-27 12:18:59 +0000

    Automatically setting up SSL now

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -34,21 +34,11 @@ resource "local_file" "global_variables" {
 }
 
 resource "local_file" "OpenOnDemandOverrides" {
-  content = templatefile("playbooks/templates/ood-overrides.yml.tmpl",
+  content = templatefile("playbooks/templates/ood-overrides-https.yml.tmpl",
     {
-      jumpbox-pip    = azurerm_public_ip.jumpbox-pip.ip_address,
-      jumpbox-user   = azurerm_linux_virtual_machine.jumpbox.admin_username,
-      scheduler-ip   = azurerm_network_interface.scheduler-nic.private_ip_address,
-      scheduler-user = azurerm_linux_virtual_machine.scheduler.admin_username,
       ondemand-fqdn  = azurerm_public_ip.ondemand-pip.fqdn,
-      ondemand-ip    = azurerm_network_interface.ondemand-nic.private_ip_address,
-      ondemand-user  = azurerm_linux_virtual_machine.ondemand.admin_username,
-      ad-ip          = azurerm_network_interface.ad-nic.private_ip_address,
-      ad-passwd      = azurerm_windows_virtual_machine.ad.admin_password,
-      anf-home-ip    = element(azurerm_netapp_volume.home.mount_ip_addresses, 0),
-      anf-home-path  = azurerm_netapp_volume.home.volume_path,
     }
   )
-  filename = "playbooks/ood-overrides.yml"
+  filename = "playbooks/ood-overrides-https.yml"
 }
 
