e48278dd8b1a879a8cf0399123f960a4e93c0abe
Hugo Meiland <humeilan@microsoft.com>
2020-12-03 12:00:52 +0100

    change vm size to upper case

--- tf/ad.tf
+++ tf/ad.tf
@@ -14,7 +14,7 @@ resource "azurerm_windows_virtual_machine" "ad" {
   name                = "ad"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_d2s_v3"
+  size                = "Standard_D2s_v3"
   admin_username      = var.admin_username
   admin_password      = random_password.password.result 
   network_interface_ids = [
--- tf/ondemand.tf
+++ tf/ondemand.tf
@@ -23,7 +23,7 @@ resource "azurerm_linux_virtual_machine" "ondemand" {
   name                = "ondemand"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_d2s_v3"
+  size                = "Standard_D2s_v3"
   admin_username      = var.admin_username
   network_interface_ids = [
     azurerm_network_interface.ondemand-nic.id,
