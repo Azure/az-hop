cfb7a2015605cf5ef1c2a699495faec483b90904
Hugo Meiland <humeilan@microsoft.com>
2020-12-06 13:06:54 +0100

    grouping for both hpc and htc jobs

--- playbooks/roles/cyclecloud/files/2-installpbs.sh
+++ playbooks/roles/cyclecloud/files/2-installpbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://github.com/PBSPro/pbspro/releases/download/v19.1.1/pbspro_19.1.1.centos7.zip
-unzip pbspro_19.1.1.centos7.zip
+unzip -o pbspro_19.1.1.centos7.zip
 yum install epel-release -y
 yum install -y pbspro_19.1.1.centos7/pbspro-execution-19.1.1-0.x86_64.rpm jq
 
@@ -11,9 +11,16 @@ sed -i 's/CHANGE_THIS_TO_PBS_PRO_SERVER_HOSTNAME/scheduler/' /var/spool/pbs/mom_
 
 # Retrieve the VMSS name to be used as the pool name for multiple VMSS support
 poolName=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2018-10-01" | jq -r '.compute.vmScaleSetName')
-/opt/pbs/bin/qmgr -c "c n $(hostname)" || exit 1
+/opt/pbs/bin/qmgr -c "c n $(hostname)" 
 /opt/pbs/bin/qmgr -c "s n $(hostname) resources_available.slot_type=$(jetpack config pbspro.slot_type)" || exit 1
-/opt/pbs/bin/qmgr -c "s n $(hostname) resources_available.ungrouped=false" || exit 1
+
+# properly set grouping for pcs/htc jobs
+if [ `jetpack config pbspro.is_grouped` == "False" ]; then
+  /opt/pbs/bin/qmgr -c "s n $(hostname) resources_available.ungrouped=true" || exit 1
+else
+  /opt/pbs/bin/qmgr -c "s n $(hostname) resources_available.ungrouped=false" || exit 1
+fi
+
 /opt/pbs/bin/qmgr -c "s n $(hostname) resources_available.group_id=${poolName}" || exit 1
 
 systemctl restart pbs
