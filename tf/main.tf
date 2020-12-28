40b58b4d56f3ef2b2171b7665b6a58de2b3bf9db
xpillons <xpillons@microsoft.com>
2020-12-28 10:49:10 +0000

    add strong version requirements

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.41.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+
 provider "azurerm" {
   features {}
 }
