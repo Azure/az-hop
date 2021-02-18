4e33d7e3a0111a30f49813ec163ca5a7f249a02b
Hugo Meiland <humeilan@microsoft.com>
2021-02-18 10:26:51 +0100

    fixes for cc healthcheck

--- playbooks/roles/cyclecloud/files/1-healthcheck.sh
+++ playbooks/roles/cyclecloud/files/1-healthcheck.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-# enable the healthcheck on the node
 
+# fixing healthcheck on cc8
+sed -i '263s/stdout=subprocess.PIPE/stdout=subprocess.PIPE, text=True/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/healthservice.py
+sed -i '46s/cwd=self.scratch/cwd=self.scratch, text=True/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/check.py
+sed -i '50s/stderr=subprocess.PIPE/stderr=subprocess.PIPE, text=True/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/check.py
+
+# enable the healthcheck on the node
 cp ../files/check_stuff.sh /opt/cycle/jetpack/config/healthcheck.d/check_stuff.sh
 chmod 777 /opt/cycle/jetpack/config/healthcheck.d/check_stuff.sh
 
+systemctl restart healthcheck
 
 
--- playbooks/roles/cyclecloud/files/check_stuff.sh
+++ playbooks/roles/cyclecloud/files/check_stuff.sh
@@ -3,3 +3,6 @@
 CURRENT_DATE=`date`
 
 echo $CURRENT_DATE >> /tmp/health_info.txt
+>&2 echo "stderr: script error happened!"
+echo "stdout: script error happened!"
+exit 10
