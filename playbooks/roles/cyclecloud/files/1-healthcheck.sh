b91522a6e489f7002d740db9fd7aa0afa0ecc379
Hugo Meiland <humeilan@microsoft.com>
2021-02-19 15:20:31 +0100

    ready to test

--- playbooks/roles/cyclecloud/files/1-healthcheck.sh
+++ playbooks/roles/cyclecloud/files/1-healthcheck.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # fixing healthcheck on cc8
-sed -i '263s/stdout=subprocess.PIPE/stdout=subprocess.PIPE, text=True/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/healthservice.py
-sed -i '46s/cwd=self.scratch/cwd=self.scratch, text=True/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/check.py
-sed -i '50s/stderr=subprocess.PIPE/stderr=subprocess.PIPE, text=True/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/check.py
+sed -i '263s/stdout=subprocess.PIPE)/stdout=subprocess.PIPE, text=True)/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/healthservice.py
+sed -i '46s/cwd=self.scratch)/cwd=self.scratch, text=True)/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/check.py
+sed -i '50s/stderr=subprocess.PIPE)/stderr=subprocess.PIPE, text=True)/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/check.py
 
 # enable the healthcheck on the node
 cp ../files/check_stuff.sh /opt/cycle/jetpack/config/healthcheck.d/check_stuff.sh
+cp ../files/healthchecks.json /opt/cycle/jetpack/config/healthchecks.json
 chmod 777 /opt/cycle/jetpack/config/healthcheck.d/check_stuff.sh
 
 systemctl restart healthcheck
+
+# force run once on startup
+source /opt/cycle/jetpack/system/bin/cyclecloud-env.sh
+healthcheck
 
 
