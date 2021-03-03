7b5d2e8b085b98e00549ff05acd4a0cb4ee39a90
Xavier Pillons <xpillons@microsoft.com>
2021-03-03 11:01:18 +0000

    Owner role can't be used for access policy

--- tf/keyvault.tf
+++ tf/keyvault.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
-data "azurerm_role_definition" "owner" {
-  name = "Owner"
-}
+# data "azurerm_role_definition" "owner" {
+#   name = "Owner"
+# }
 
 resource "azurerm_key_vault" "azhop" {
   name                        = format("%s%s", "kv", random_string.resource_postfix.result)
@@ -37,22 +37,23 @@ resource "azurerm_key_vault_access_policy" "admin" {
     ]
 }
 
-resource "azurerm_key_vault_access_policy" "owner" {
+# Access Policy can only be granted on Principals and not Roles
+# resource "azurerm_key_vault_access_policy" "owner" {
 
-  key_vault_id = azurerm_key_vault.azhop.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_role_definition.owner.id
+#   key_vault_id = azurerm_key_vault.azhop.id
+#   tenant_id    = data.azurerm_client_config.current.tenant_id
+#   object_id    = regex("[0-9a-f-]{36}.?", data.azurerm_role_definition.owner.id)
 
-  secret_permissions = [
-      "get",
-      "set",
-      "list",
-      "delete",
-      "purge",
-      "recover",
-      "restore"
-    ]
-}
+#   secret_permissions = [
+#       "get",
+#       "set",
+#       "list",
+#       "delete",
+#       "purge",
+#       "recover",
+#       "restore"
+#     ]
+# }
 
 resource "azurerm_key_vault_access_policy" "reader" {
 
