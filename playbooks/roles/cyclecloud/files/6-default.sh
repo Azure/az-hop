08516c448fc20472bfdc08ef8e52e087e73ec065
Xavier Pillons <xpillons@microsoft.com>
2021-03-12 14:48:33 +0000

    allow domain users to run sudo with no password

--- playbooks/roles/cyclecloud/files/6-default.sh
+++ playbooks/roles/cyclecloud/files/6-default.sh
@@ -3,3 +3,6 @@
 
 # change access to resource so that temp jobs can be written there
 chmod 777 /mnt/resource
+
+# Grant domain users sudo with no password
+echo "\"%domain users\" ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers
