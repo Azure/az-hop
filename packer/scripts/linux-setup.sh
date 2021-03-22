74bc5960ae880a99744137911d8a89e4e0559dae
Xavier Pillons <xpillons@microsoft.com>
2021-03-22 14:31:08 +0000

    add python3 and azcopy

--- packer/scripts/linux-setup.sh
+++ packer/scripts/linux-setup.sh
@@ -2,7 +2,7 @@
 
 yum install -y sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python
 yum install -y nfs-utils openssl-devel epel-release
-yum install -y patch gcc gcc-c++ perl-Data-Dumper perl-Thread-Queue Lmod hwloc numactl jq htop
+yum install -y patch gcc gcc-c++ perl-Data-Dumper perl-Thread-Queue Lmod hwloc numactl jq htop python3
 
 # Disable requiretty to allow run sudo within scripts
 sed -i -e 's/Defaults    requiretty.*/ #Defaults    requiretty/g' /etc/sudoers
@@ -17,3 +17,8 @@ cat << EOF >> /etc/security/limits.conf
 *               hard    nofile          65535
 *               soft    nofile          65535
 EOF
+
+# Install azcopy
+cd /usr/local/bin
+wget -q https://aka.ms/downloadazcopy-v10-linux -O - | tar zxf - --strip-components 1 --wildcards '*/azcopy'
+chmod 755 /usr/local/bin/azcopy
