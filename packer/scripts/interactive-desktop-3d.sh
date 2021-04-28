00555e1692e63ea430bf0c6d5f4e7765fe073cbe
Xavier Pillons <xpillons@microsoft.com>
2021-04-28 14:40:48 +0000

    set fix version for CUDA

--- packer/scripts/interactive-desktop-3d.sh
+++ packer/scripts/interactive-desktop-3d.sh
@@ -11,8 +11,7 @@ blacklist lbm-nouveau
 EOF
 
 
-################### INSTALL VirtualGL / VNC
-
+echo "################### INSTALL VirtualGL / VNC"
 yum groupinstall -y "X Window system"
 yum groupinstall -y xfce
 yum install -y https://netix.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc-2.2.5.x86_64.rpm
@@ -25,11 +24,12 @@ systemctl disable firstboot-graphical
 systemctl set-default graphical.target
 systemctl isolate graphical.target
 
-# install CUDA
+echo "################### INSTALL CUDA"
 NVIDIA_DRIVER_VERSION=460.32.03
+CUDA_VERSION=11-2
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum clean all
-yum -y install nvidia-driver-latest-dkms-$NVIDIA_DRIVER_VERSION cuda
+yum -y install nvidia-driver-latest-dkms-$NVIDIA_DRIVER_VERSION cuda-$CUDA_VERSION
 yum -y install cuda-drivers-$NVIDIA_DRIVER_VERSION
 
 # browser and codecs
@@ -42,11 +42,11 @@ net.core.rmem_max=2097152
 net.core.wmem_max=2097152
 EOF
 
-################### INSTALL NVIDIA DRIVERS
+echo "################### INSTALL NVIDIA GRID DRIVERS"
 
 init 3
 # Use the direct link which contains the clear version number
-wget -O NVIDIA-Linux-x86_64-grid.run https://download.microsoft.com/download/9/5/c/95c667ff-ab95-4c56-89e0-e13e9a76782d/NVIDIA-Linux-x86_64-460.32.03-grid-azure.run
+wget -O NVIDIA-Linux-x86_64-grid.run https://download.microsoft.com/download/9/5/c/95c667ff-ab95-4c56-89e0-e13e9a76782d/NVIDIA-Linux-x86_64-$NVIDIA_DRIVER_VERSION-grid-azure.run
 chmod +x NVIDIA-Linux-x86_64-grid.run
 sudo ./NVIDIA-Linux-x86_64-grid.run -s
 # Answers are: yes, yes, yes
