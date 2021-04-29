59c018fe8757657d36a54999058264dfca3bf517
Xavier Pillons <xpillons@microsoft.com>
2021-04-29 09:54:48 +0000

    reorganize cycle roles

--- playbooks/roles/cyclecloud_pbs/cluster-init/scripts/1-healthcheck.sh
+++ playbooks/roles/cyclecloud_pbs/cluster-init/scripts/1-healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# fixing healthcheck on cc8
+# fixing healthcheck on cc8 => Does this is needed with 8.1.1 ?
 sed -i '263s/stdout=subprocess.PIPE)/stdout=subprocess.PIPE, text=True)/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/healthservice.py
 sed -i '46s/cwd=self.scratch)/cwd=self.scratch, text=True)/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/check.py
 sed -i '50s/stderr=subprocess.PIPE)/stderr=subprocess.PIPE, text=True)/' /opt/cycle/jetpack/system/embedded/lib/python3.8/site-packages/healthcheck/check.py
