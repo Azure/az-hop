374fba6c6861633b66dad2f6bb66efccc14c1e6f
Xavier Pillons <xpillons@microsoft.com>
2021-04-29 10:17:34 +0000

    reinit the session for NV* instances

--- playbooks/roles/cyclecloud_pbs/cluster-init/scripts/6-default.sh
+++ playbooks/roles/cyclecloud_pbs/cluster-init/scripts/6-default.sh
@@ -6,3 +6,12 @@ chmod 777 /mnt/resource
 
 # Grant domain users sudo with no password
 echo "\"%domain users\" ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers
+
+# For any NV instances, reinit the session
+AZHPC_VMSIZE=$(curl -s --noproxy "*" -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2019-08-15" | jq -r '.vmSize' | tr '[:upper:]' '[:lower:]')
+case $AZHPC_VMSIZE in
+  standard_nv*)
+    init 3
+    init 5
+  ;;
+esac
