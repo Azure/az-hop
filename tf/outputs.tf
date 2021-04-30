e1873d9ae8c7284fd3327f83b7e95fec8f530c4b
Xavier Pillons <xpillons@microsoft.com>
2021-04-30 16:08:28 +0200

    Revert "Bugfix for incorrect hsm container name"

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -35,7 +35,7 @@ resource "local_file" "global_variables" {
       key_vault           = azurerm_key_vault.azhop.name
       sig_name            = azurerm_shared_image_gallery.sig.name
       lustre_hsm_storage_account = ( local.lustre_archive_account != null ? local.lustre_archive_account : azurerm_storage_account.azhop.name )
-      lustre_hsm_storage_container = ( local.lustre_archive_account != null ? local.configuration_yml["lustre"]["hsm"]["storage_container"] : azurerm_storage_container.lustre_archive.name )
+      lustre_hsm_storage_container = ( local.lustre_archive_account != null ? local.configuration_yml["lustre"]["hsm"]["storage_container"] : azurerm_storage_account.azhop.name )
     }
   )
   filename = "${local.playbook_root_dir}/group_vars/all.yml"
