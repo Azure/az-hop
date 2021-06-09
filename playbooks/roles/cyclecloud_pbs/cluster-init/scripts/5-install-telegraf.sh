ffa14a4f816ae12060e821f9ade0e40642eda294
Xavier Pillons <xpillons@microsoft.com>
2021-06-09 22:06:35 +0000

    global tags and input tuning

--- playbooks/roles/cyclecloud_pbs/cluster-init/scripts/5-install-telegraf.sh
+++ playbooks/roles/cyclecloud_pbs/cluster-init/scripts/5-install-telegraf.sh
@@ -17,6 +17,13 @@ if ! rpm -q telegraf; then
   yum -y install https://dl.influxdata.com/telegraf/releases/telegraf-1.18.2-1.x86_64.rpm
 fi
 
+echo "Configuring global tags"
+AZHPC_VMSIZE=$(curl -s --noproxy "*" -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2019-08-15" | jq -r '.vmSize' | tr '[:upper:]' '[:lower:]')
+PHYSICAL_HOST=$(strings /var/lib/hyperv/.kvp_pool_3 | grep -A1 PhysicalHostName | head -n 2 | tail -1)
+
+sed -i "s/__SKU__/${AZHPC_VMSIZE}/g" ../files/telegraf.conf
+sed -i "s/__PHYS_HOST__/${PHYSICAL_HOST}/g" ../files/telegraf.conf
+
 echo "Copy configuration file to use"
 TELEGRAF_CONF_DIR=/etc/telegraf
 cp ../files/telegraf.conf $TELEGRAF_CONF_DIR/telegraf.conf
