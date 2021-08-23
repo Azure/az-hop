d64fb400bda749d73c4f5b1ce3bff95147dd9192
hugo meiland <hugo.meiland@microsoft.com>
2021-08-23 15:53:28 +0200

    stage 3,4

--- toolset/stages/stage3.sh
+++ toolset/stages/stage3.sh
@@ -4,9 +4,23 @@ export TF_BACKEND_FILE=$TF_FOLDER/backend.tf
 export TF_CLI_ARGS_apply="-auto-approve"
 
 echo Stage 3: creating ccportal, scheduler and sig
-mv tf_stage2/terraform.tfstate tf_stage3/
-terraform -chdir=./tf_stage3 init
-terraform -chdir=./tf_stage3 apply
+mkdir -p ./tf/stage3
+pushd tf/stage3
+ln -sf ../main.tf
+ln -sf ../network.tf
+ln -sf ../variables.tf
+ln -sf ../variables_local.tf
+ln -sf ../ad.tf
+ln -sf ../jumpbox.tf
+ln -sf ../keyvault.tf
+ln -sf ../sig.tf
+ln -sf ../ccportal.tf
+ln -sf ../scheduler.tf
+ln -sf ../stage2/anf.tf
+popd
+mv tf/stage2/terraform.tfstate tf/stage3/
+terraform -chdir=./tf/stage3 init
+terraform -chdir=./tf/stage3 apply
 ansible-playbook -i playbooks/inventory ./playbooks/linux.yml
 ansible-playbook -i playbooks/inventory ./playbooks/ccportal.yml
 ansible-playbook -i playbooks/inventory ./playbooks/ccpbs.yml
--- toolset/stages/stage4.sh
+++ toolset/stages/stage4.sh
@@ -4,6 +4,21 @@ export TF_BACKEND_FILE=$TF_FOLDER/backend.tf
 export TF_CLI_ARGS_apply="-auto-approve"
 
 echo Stage 4: create LustreFS
+mkdir -p ./tf/stage3
+pushd tf/stage3
+ln -sf ../main.tf
+ln -sf ../network.tf
+ln -sf ../variables.tf
+ln -sf ../variables_local.tf
+ln -sf ../ad.tf
+ln -sf ../jumpbox.tf
+ln -sf ../keyvault.tf
+ln -sf ../sig.tf
+ln -sf ../ccportal.tf
+ln -sf ../scheduler.tf
+ln -sf ../lustre.tf
+ln -sf ../stage2/anf.tf
+popd
 mv tf_stage3/terraform.tfstate tf_stage4/
 terraform -chdir=./tf_stage4 init
 terraform -chdir=./tf_stage4 apply
