a32228bcad5bd07ff744ff0576797681f0c6b642
hugo meiland <hugo.meiland@microsoft.com>
2021-08-23 12:29:04 +0200

    statge 0

--- toolset/stages/stage0.sh
+++ toolset/stages/stage0.sh
@@ -4,5 +4,12 @@ export TF_BACKEND_FILE=$TF_FOLDER/backend.tf
 export TF_CLI_ARGS_apply="-auto-approve"
 
 echo Stage 0: creating ResourceGroup, StorageAccount and VNet
-terraform -chdir=./tf_stage0 init
-terraform -chdir=./tf_stage0 apply
+mkdir -p ./tf/stage0
+pushd tf/stage0
+ln -sf ../main.tf
+ln -sf ../network.tf
+ln -sf ../variables.tf
+ln -sf ../variables_local.tf
+popd
+terraform -chdir=./tf/stage0 init
+terraform -chdir=./tf/stage0 apply
