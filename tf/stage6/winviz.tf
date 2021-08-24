c39f0bbeb2486bd3ea3cf0844a18c225fd4f4b2a
hugo meiland <hugo.meiland@microsoft.com>
2021-08-24 16:13:24 +0200

    stage6 windows viz node

---
+++ tf/stage6/winviz.tf
@@ -0,0 +1,40 @@
+resource "azurerm_network_interface" "win-nic" {
+  name                = "win-nic"
+  resource_group_name = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
+  location            = local.create_rg ? azurerm_resource_group.rg[0].location : data.azurerm_resource_group.rg[0].location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = local.create_vnet ? azurerm_subnet.admin[0].id : data.azurerm_subnet.admin[0].id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "win" {
+  name                = "win"
+  resource_group_name = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
+  location            = local.create_rg ? azurerm_resource_group.rg[0].location : data.azurerm_resource_group.rg[0].location
+  size                = try(local.configuration_yml["win"].vm_size, "Standard_D2s_v3")
+  admin_username      = local.admin_username
+  admin_password      = random_password.password.result 
+  network_interface_ids = [
+    azurerm_network_interface.win-nic.id,
+  ]
+
+  winrm_listener {
+    protocol = "Http"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter-smalldisk"
+    version   = "latest"
+  }
+}
+
