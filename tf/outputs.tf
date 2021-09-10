374a326d787e0f5fb4514a979ab6f2e3c3541fb3
Xavier Pillons <xpillons@microsoft.com>
2021-09-10 11:05:45 +0000

    configure Packer for no PIP

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -72,6 +72,10 @@ resource "local_file" "packer" {
       resource_group  = local.resource_group
       location        = local.location
       sig_name        = azurerm_shared_image_gallery.sig.name
+      private_virtual_network_with_public_ip = local.allow_public_ip
+      virtual_network_name                   = local.create_vnet ? azurerm_virtual_network.azhop[0].name : data.azurerm_virtual_network.azhop[0].name
+      virtual_network_subnet_name            = local.create_vnet ? azurerm_subnet.admin[0].name : data.local.create_vnet ? azurerm_subnet.admin[0].name
+      virtual_network_resource_group_name    = local.create_vnet ? azurerm_virtual_network.azhop[0].resource_group_name : data.azurerm_virtual_network.azhop[0].resource_group_name
     }
   )
   filename = "${local.packer_root_dir}/options.json"
