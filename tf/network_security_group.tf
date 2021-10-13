0c7d526f08259c7f2b875d0cf9e0ab19a57dc0c8
Xavier Pillons <xpillons@microsoft.com>
2021-10-13 13:18:31 +0000

    Create NSGs all in once

--- tf/network_security_group.tf
+++ tf/network_security_group.tf
@@ -27,33 +27,32 @@ data "azurerm_subnet" "subnets" {
   virtual_network_name = local.create_vnet ? azurerm_virtual_network.azhop[0].name : data.azurerm_virtual_network.azhop[0].name
 }
 
-# Network security group for all subnet
+# Network security group for all subnets, always create this resource so we can add dynamic content
 resource "azurerm_network_security_group" "common" {
-  count                = local.create_nsg ? 1 : 0
   name                = "nsg-common"
   resource_group_name = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
   location            = local.create_rg ? azurerm_resource_group.rg[0].location : data.azurerm_resource_group.rg[0].location
-}
 
-resource "azurerm_network_security_rule" "all_rules"  {
+  dynamic "security_rule" {
     for_each = local.create_nsg ? local.nsg_rules : local.empty_map
-    resource_group_name = azurerm_network_security_group.common[0].resource_group_name
-    network_security_group_name = azurerm_network_security_group.common[0].name
-    name                       = each.key
-    priority                   = each.value[0]
-    direction                  = each.value[1]
-    access                     = each.value[2]
-    protocol                   = each.value[3]
-    source_port_range          = "*"
-    destination_port_ranges    = local.nsg_destination_ports[each.value[4]]
+    content {
+      name                       = security_rule.key
+      priority                   = security_rule.value[0]
+      direction                  = security_rule.value[1]
+      access                     = security_rule.value[2]
+      protocol                   = security_rule.value[3]
+      source_port_range          = "*"
+      destination_port_ranges    = local.nsg_destination_ports[security_rule.value[4]]
 
-    source_address_prefix                 = try(split("/", each.value[5])[0] == "tag" ? split("/", each.value[5])[1] : null, null)
-    source_application_security_group_ids = split("/", each.value[5])[0] == "asg" ? [azurerm_application_security_group.asg[split("/", each.value[5])[1]].id] : []
-    source_address_prefixes               = try(split("/", each.value[5])[0] == "subnet" ? data.azurerm_subnet.subnets[split("/", each.value[5])[1]].address_prefixes : null, null)
+      source_address_prefix                 = try(split("/", security_rule.value[5])[0] == "tag" ? split("/", security_rule.value[5])[1] : null, null)
+      source_application_security_group_ids = split("/", security_rule.value[5])[0] == "asg" ? [azurerm_application_security_group.asg[split("/", security_rule.value[5])[1]].id] : []
+      source_address_prefixes               = try(split("/", security_rule.value[5])[0] == "subnet" ? data.azurerm_subnet.subnets[split("/", security_rule.value[5])[1]].address_prefixes : null, null)
 
-    destination_address_prefix                 = try(split("/", each.value[6])[0] == "tag" ? split("/", each.value[6])[1] : null, null)
-    destination_application_security_group_ids = split("/", each.value[6])[0] == "asg" ? [azurerm_application_security_group.asg[split("/", each.value[6])[1]].id] : []
-    destination_address_prefixes               = try(split("/", each.value[6])[0] == "subnet" ? data.azurerm_subnet.subnets[split("/", each.value[6])[1]].address_prefixes : null, null)
+      destination_address_prefix                 = try(split("/", security_rule.value[6])[0] == "tag" ? split("/", security_rule.value[6])[1] : null, null)
+      destination_application_security_group_ids = split("/", security_rule.value[6])[0] == "asg" ? [azurerm_application_security_group.asg[split("/", security_rule.value[6])[1]].id] : []
+      destination_address_prefixes               = try(split("/", security_rule.value[6])[0] == "subnet" ? data.azurerm_subnet.subnets[split("/", security_rule.value[6])[1]].address_prefixes : null, null)
+    }
+  }
 }
 
 # NSG cannot be applied on a delegated subnet for Azure Netapp files https://docs.microsoft.com/en-us/azure/azure-netapp-files/azure-netapp-files-delegate-subnet, nor on Bastion
@@ -61,23 +60,23 @@ resource "azurerm_network_security_rule" "all_rules"  {
 resource "azurerm_subnet_network_security_group_association" "frontend" {
   count                     = local.create_nsg ? 1 : 0
   subnet_id                 = azurerm_subnet.frontend[count.index].id
-  network_security_group_id = azurerm_network_security_group.common[count.index].id
+  network_security_group_id = azurerm_network_security_group.common.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "ad" {
   count                     = local.create_nsg ? 1 : 0
   subnet_id                 = azurerm_subnet.ad[count.index].id
-  network_security_group_id = azurerm_network_security_group.common[count.index].id
+  network_security_group_id = azurerm_network_security_group.common.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "compute" {
   count                     = local.create_nsg ? 1 : 0
   subnet_id                 = azurerm_subnet.compute[count.index].id
-  network_security_group_id = azurerm_network_security_group.common[count.index].id
+  network_security_group_id = azurerm_network_security_group.common.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "admin" {
   count                     = local.create_nsg ? 1 : 0
   subnet_id                 = azurerm_subnet.admin[count.index].id
-  network_security_group_id = azurerm_network_security_group.common[count.index].id
+  network_security_group_id = azurerm_network_security_group.common.id
 }
