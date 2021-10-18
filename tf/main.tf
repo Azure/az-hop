c6a1058ba9f8f389a0747b18f380bdda8030aba9
Xavier Pillons <xpillons@microsoft.com>
2021-10-18 11:25:37 +0000

    bump version from 2.61 to 2.81

--- tf/main.tf
+++ tf/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.61.0"
+      version = "~> 2.81.0"
     }
     random = {
       source  = "hashicorp/random"
