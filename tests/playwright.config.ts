687991dc726be4b398efa1a49dd244db762783be
Xavier Pillons <xpillons@microsoft.com>
2021-12-16 20:30:05 +0100

    upload test results

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,8 +1,11 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
+    testDir: './tests',
+    /* Maximum time one test can run for. */
+    timeout: 30 * 1000,
     globalTimeout: 60 * 60 * 1000,
-    snapshotDir: 'snapshots',
-    reporter: process.env.CI ? 'github' : 'list',
+    snapshotDir: './snapshots',
+    reporter: 'html', //process.env.CI ? 'github' : 'list',
     use: {
         headless: true,
         baseURL: process.env.AZHOP_FQDN,
