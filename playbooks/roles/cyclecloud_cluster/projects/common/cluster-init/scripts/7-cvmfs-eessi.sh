2221be87100198948a1dc6eaef7d5cd1fd88e94e
Xavier Pillons <xpillons@microsoft.com>
2022-03-15 19:54:08 +0100

    remove sudo

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/7-cvmfs-eessi.sh
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/7-cvmfs-eessi.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
-sudo yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
-sudo yum install -y cvmfs
-sudo yum install -y https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi-latest.noarch.rpm
+yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
+yum install -y cvmfs
+yum install -y https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi-latest.noarch.rpm
 
 # configure CernVM-FS (no proxy, 10GB quota for CernVM-FS cache)
-sudo bash -c "echo 'CVMFS_HTTP_PROXY=DIRECT' > /etc/cvmfs/default.local"
-sudo bash -c "echo 'CVMFS_QUOTA_LIMIT=10000' >> /etc/cvmfs/default.local"
-sudo cvmfs_config setup
-
+bash -c "echo 'CVMFS_HTTP_PROXY=DIRECT' > /etc/cvmfs/default.local"
+bash -c "echo 'CVMFS_QUOTA_LIMIT=10000' >> /etc/cvmfs/default.local"
+ cvmfs_config setup
