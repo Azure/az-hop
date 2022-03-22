d85bb2af935b70d85814602a2b90cd66e1f99d25
Xavier Pillons <xpillons@microsoft.com>
2022-03-22 13:43:45 +0100

    ssh port when building ANF in dual protocol

--- tf/active_directory/outputs.tf
+++ tf/active_directory/outputs.tf
@@ -3,6 +3,7 @@ resource "local_file" "AnsibleInventory" {
    {
       jumpbox-pip       = local.allow_public_ip ? azurerm_public_ip.jumpbox-pip[0].ip_address : azurerm_network_interface.jumpbox-nic.private_ip_address
       jumpbox-user      = azurerm_linux_virtual_machine.jumpbox.admin_username
+      jumpbox-ssh-port  = local.jumpbox_ssh_port
       ad-ip             = azurerm_network_interface.ad-nic.private_ip_address
       ad-passwd         = azurerm_windows_virtual_machine.ad.admin_password
     }
@@ -22,7 +23,22 @@ resource "local_file" "global_variables" {
       config_file         = local.configuration_file
       ad-ip               = azurerm_network_interface.ad-nic.private_ip_address
       key_vault           = azurerm_key_vault.azhop.name
+      jumpbox-ssh-port    = local.jumpbox_ssh_port      
     }
   )
   filename = "${local.playbook_root_dir}/group_vars/all.yml"
+}
+
+resource "local_file" "ci_jumpbox" {
+  content = templatefile("${local.playbooks_template_dir}/jumpbox_ci.tmpl",
+    {
+      jumpbox-ssh-port  = local.jumpbox_ssh_port
+    }
+  )
+  filename = "${path.root}/cloud-init/jumpbox.yml"
+}
+
+data "local_file" "ci_jumpbox" {
+  filename = "${path.root}/cloud-init/jumpbox.yml"
+  depends_on = [local_file.ci_jumpbox]
 }
