a16633b0507892596f34fccdca2246f666222654
Xavier Pillons <xpillons@microsoft.com>
2022-03-31 19:02:29 +0200

    add domain users to the RDP group

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/4-configure-rdp.bat
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/4-configure-rdp.bat
@@ -1,9 +1,6 @@
 reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 netsh advfirewall firewall add rule name="allow RemoteDesktop" dir=in protocol=TCP localport=3389 action=allow
 
-Powershell.exe -executionpolicy remotesigned try { Add-LocalGroupMember -Group "Remote Desktop Users" -Member "HPC\Domain Users" } catch { exit 255 }
+net localgroup "Remote Desktop Users" "HPC\Domain Users" /add
 
-if %ERRORLEVEL% neq 0 ( 
-   exit 1
-)
 
