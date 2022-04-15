e46c987fbab572ef34c96d451950af734e8df1db
Xavier Pillons <xpillons@microsoft.com>
2022-04-15 11:02:59 +0200

    add guacamole in the install all

--- install.sh
+++ install.sh
@@ -75,6 +75,7 @@ case $TARGET in
     run_playbook scheduler
     run_playbook ood $PLAYBOOKS_DIR/ood-overrides-common.yml $PLAYBOOKS_DIR/ood-overrides-$SCHEDULER.yml $PLAYBOOKS_DIR/ood-overrides-auth-$OOD_AUTH.yml
     run_playbook ood-custom
+    run_playbook guacamole
     run_playbook grafana 
     run_playbook telegraf
     run_playbook chrony
