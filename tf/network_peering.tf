fedf217f1169b473f5c0ae05971cd4c6ff26fbd3
Xavier Pillons <xpillons@microsoft.com>
2022-05-03 16:31:09 +0200

    rename peering

--- tf/network_peering.tf
+++ tf/network_peering.tf
@@ -6,7 +6,7 @@ data "azurerm_virtual_network" "peernetwork" {
 
 resource "azurerm_virtual_network_peering" "azhop-to-peer" {
   count                         = length(local.vnet_peering)
-  name                          = "azhop-to-${local.vnet_peering[count.index].vnet_name}"
+  name                          = local.vnet_peering[count.index].vnet_resource_group #"azhop-to-${local.vnet_peering[count.index].vnet_name}"
   resource_group_name           = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
   virtual_network_name          = azurerm_virtual_network.azhop[0].name
   remote_virtual_network_id     = data.azurerm_virtual_network.peernetwork[count.index].id
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network_peering" "azhop-to-peer" {
 
 resource "azurerm_virtual_network_peering" "peer-to-azhop" {
   count                         = length(local.vnet_peering)
-  name                          = "${local.vnet_peering[count.index].vnet_name}-to-azhop"
+  name                          = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name #"${local.vnet_peering[count.index].vnet_name}-to-azhop"
   resource_group_name           = local.vnet_peering[count.index].vnet_resource_group
   virtual_network_name          = local.vnet_peering[count.index].vnet_name
   remote_virtual_network_id     = azurerm_virtual_network.azhop[0].id
