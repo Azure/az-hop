b3bf36a91ea729235ef456bc3ef2a48a16cca9ef
Xavier Pillons <xpillons@microsoft.com>
2022-05-20 12:31:58 +0200

    add local admins

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/files/windows/4-configure-rdp.bat
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/files/windows/4-configure-rdp.bat
@@ -1,10 +1,12 @@
-:: C:\cycle\jetpack\bin\jetpack log "Update RDP configuration" --level info --priority low
-
-:: C:\cycle\jetpack\bin\jetpack log "Update registry" --level info --priority low
 :: reg add "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d 3390 /f
 reg add "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 :: netsh advfirewall firewall add rule name="allow RemoteDesktop" dir=in protocol=TCP localport=3389 action=allow
 
+:: Allow paste of password on UAC windows
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v PromptOnSecureDesktop /t REG_DWORD /d 0 /f 
+
+net localgroup "Administrators" "HPC\az-hop-localadmins" /add
+
 net localgroup "Remote Desktop Users" "HPC\Domain Users" /add
 net localgroup "Remote Desktop Users"
-:: C:\cycle\jetpack\bin\jetpack log "End of configure rdp script" --level info --priority low
+:: call C:\cycle\jetpack\bin\jetpack log "End of configure rdp script" --level info --priority low
