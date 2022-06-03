1aa350f17e6d8e37c883b85bacf5c5ff8c934a92
Xavier Pillons <xpillons@microsoft.com>
2022-06-03 09:45:27 +0200

    Added Windows Desktop test

--- tests/ood_interactive_apps.spec.ts
+++ tests/ood_interactive_apps.spec.ts
@@ -1,7 +1,74 @@
 import {test, expect} from '@playwright/test';
 import config from './playwright.config';
 
-test('Interactive Apps', async ({browser}) => {
+test('Windows Desktop', async ({browser}) => {
+    // create context with HTTP credentials
+    const context = await browser.newContext();
+    // Create a page
+    const page = await context.newPage();
+    await page.goto('/', { waitUntil: 'networkidle' });
+
+    // Click text=Interactive Apps
+    await page.click('text=Interactive Apps');
+
+    // Click div[role="main"] >> text=Windows Desktop
+    await page.click('div[role="main"] >> text=Windows Desktop');
+
+    // Click input:has-text("Launch")
+    await page.click('input:has-text("Launch")');
+
+    // Click a:has-text("Delete")
+    await page.click('a:has-text("Delete")');
+
+    // Click text=Confirm
+    await page.click('text=Confirm');
+
+    await page.waitForLoadState('networkidle');
+    await page.close()
+    
+    // Close the browser
+    await context.close();
+});
+
+test('Linux Desktop', async ({browser}) => {
+    // create context with HTTP credentials
+    const context = await browser.newContext();
+    // Create a page
+    const page = await context.newPage();
+    await page.goto('/', { waitUntil: 'networkidle' });
+
+    // Click text=Interactive Apps
+    await page.click('text=Interactive Apps');
+
+    // Click div[role="main"] >> text=Linux Desktop
+    await page.click('div[role="main"] >> text=Linux Desktop');
+
+    // Click input[name="batch_connect_session_context\[hours\]"]
+    await page.click('input[name="batch_connect_session_context\\[num_hours\\]"]');
+
+    // Fill input[name="batch_connect_session_context\[hours\]"]
+    await page.fill('input[name="batch_connect_session_context\\[num_hours\\]"]', '1');
+
+    // Press Tab
+    await page.press('input[name="batch_connect_session_context\\[num_hours\\]"]', 'Tab');
+
+    // Click input:has-text("Launch")
+    await page.click('input:has-text("Launch")');
+
+    // Click text=Delete
+    await page.click('text=Delete');
+
+    // Click text=Confirm
+    await page.click('text=Confirm');
+
+    await page.waitForLoadState('networkidle');
+    await page.close()
+    
+    // Close the browser
+    await context.close();
+});
+
+test('CodeServer', async ({browser}) => {
     // create context with HTTP credentials
     const context = await browser.newContext();
     // Create a page
@@ -26,29 +93,8 @@ test('Interactive Apps', async ({browser}) => {
     // Click input:has-text("Launch")
     await page.click('input:has-text("Launch")');
 
-    // Click div[role="main"] >> text=Remote Desktop
-    await page.click('div[role="main"] >> text=Remote Desktop');
-
-    // Click input[name="batch_connect_session_context\[hours\]"]
-    await page.click('input[name="batch_connect_session_context\\[num_hours\\]"]');
-
-    // Fill input[name="batch_connect_session_context\[hours\]"]
-    await page.fill('input[name="batch_connect_session_context\\[num_hours\\]"]', '1');
-
-    // Press Tab
-    await page.press('input[name="batch_connect_session_context\\[num_hours\\]"]', 'Tab');
-
-    // Click input:has-text("Launch")
-    await page.click('input:has-text("Launch")');
-
     // Click text=Delete
     await page.click('text=Delete');
-
-    // Click text=Confirm
-    await page.click('text=Confirm');
-
-    // Click a:has-text("Delete")
-    await page.click('a:has-text("Delete")');
 
     // Click text=Confirm
     await page.click('text=Confirm');
@@ -59,3 +105,4 @@ test('Interactive Apps', async ({browser}) => {
     // Close the browser
     await context.close();
 });
+
