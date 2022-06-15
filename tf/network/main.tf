bfcf4909e4cbc62b1432c80a5aeafaf0c03519ad
Xavier Pillons <xpillons@microsoft.com>
2022-06-15 16:52:43 +0200

    specify custom asgs

--- tf/network/main.tf
+++ tf/network/main.tf
@@ -17,6 +17,8 @@ provider "azurerm" {
   features {}
 }
 
+data "azurerm_subscription" "primary" {}
+
 data "azurerm_resource_group" "rg" {
   count    = local.create_rg ? 0 : 1
   name     = local.resource_group
@@ -27,12 +29,13 @@ resource "azurerm_resource_group" "rg" {
   count    = local.create_rg ? 1 : 0
   name     = local.resource_group
   location = local.location
+
   tags = merge( local.common_tags, local.extra_tags)
 
   lifecycle {
     ignore_changes = [
-      tags["CreatedOn"]
+      tags
     ]
-  }
+  }  
 }
 
