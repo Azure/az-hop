3ef5a73b6eda4926674e13f7a683fe1e7a32efac
Victor Gamayunov <vgamayunov@microsoft.com>
2022-07-05 20:55:14 +0100

    remove kernel boot parameters from ubuntu

--- packer/scripts/ubuntu/enroot.sh
+++ packer/scripts/ubuntu/enroot.sh
@@ -20,9 +20,9 @@ sudo apt install -y curl gawk jq squashfs-tools parallel
 sudo apt install -y libnvidia-container-tools pigz squashfuse # optional
 
 # Add kernel boot parameters
-grep user.max_user_namespaces /etc/sysctl.conf || echo 'user.max_user_namespaces = 1417997' >> /etc/sysctl.conf
-grep namespace.unpriv_enable /etc/default/grub || sed -i.bak 's/\(GRUB_CMDLINE_LINUX.*\)"$/\1 namespace.unpriv_enable=1 user_namespace.enable=1 vsyscall=emulate"/' /etc/default/grub
-[ -e /boot/grub2/grub.cfg ] && grub2-mkconfig -o /boot/grub2/grub.cfg
-[ -e /boot/efi/EFI/centos/grub.cfg ] && grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
+# grep user.max_user_namespaces /etc/sysctl.conf || echo 'user.max_user_namespaces = 1417997' >> /etc/sysctl.conf
+# grep namespace.unpriv_enable /etc/default/grub || sed -i.bak 's/\(GRUB_CMDLINE_LINUX.*\)"$/\1 namespace.unpriv_enable=1 user_namespace.enable=1 vsyscall=emulate"/' /etc/default/grub
+# [ -e /boot/grub2/grub.cfg ] && grub2-mkconfig -o /boot/grub2/grub.cfg
+# [ -e /boot/efi/EFI/centos/grub.cfg ] && grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
 
 enroot version
