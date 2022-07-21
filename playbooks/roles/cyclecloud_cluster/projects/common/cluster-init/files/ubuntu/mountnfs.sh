c9d171a225a78c9a4dd7a10dfdd8c0a06a42d6f6
Xavier Pillons <xpillons@microsoft.com>
2022-07-21 12:51:33 +0200

    exit on mount failure

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/files/ubuntu/mountnfs.sh
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/files/ubuntu/mountnfs.sh
@@ -11,4 +11,4 @@ fi
 
 mkdir $homedir_mountpoint
 echo "mount $nfs_home_ip:/$nfs_home_path $homedir_mountpoint"
-mount $nfs_home_ip:/$nfs_home_path $homedir_mountpoint
+mount $nfs_home_ip:/$nfs_home_path $homedir_mountpoint || exit 1
