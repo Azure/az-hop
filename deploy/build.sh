9d727470c8680ce0338c638c64758920a4459837
Xavier Pillons <xpillons@microsoft.com>
2022-07-26 17:19:50 +0200

    exit on scripts errors

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 $BASEDIR/resources/generate_bicep.py -c build.yml -i $BASEDIR/resources/bicep-templates -o azureDeploy.bicep
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 rg=$1
 
 if [ "$rg" == "" ]; then
