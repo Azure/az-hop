9dd7168c6de0bad991b45697cfe1d1816cfc52e8
Xavier Pillons <xpillons@microsoft.com>
2022-08-23 15:20:08 +0200

    upgrade to terraform azurerm 3.19

--- tf/network/main.tf
+++ tf/network/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.1.0"
+      version = "=3.19.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0.0"
+      version = "~> 3.3.0"
     }
   }
   required_version = ">= 0.13"
