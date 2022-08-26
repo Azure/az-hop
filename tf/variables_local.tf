38ac50faffb6ff4f278771af27e2fb2cee37e14e
Xavier Pillons <xpillons@microsoft.com>
2022-08-26 14:15:52 +0200

    Add SMB outbound rule

--- tf/variables_local.tf
+++ tf/variables_local.tf
@@ -217,6 +217,7 @@ locals {
         Slurmd = ["6818"]
         Lustre = ["635", "988"]
         Nfs = ["111", "635", "2049", "4045", "4046"]
+        SMB = ["445"]
         Telegraf = ["8086"]
         Grafana = ["3000"]
         # HTTPS, AMQP
@@ -359,6 +360,9 @@ locals {
         # NFS
         AllowNfsOut                 = ["440", "Outbound", "Allow", "*",   "Nfs",                "asg/asg-nfs-client",       "subnet/netapp"],
         AllowNfsComputeOut          = ["450", "Outbound", "Allow", "*",   "Nfs",                "subnet/compute",           "subnet/netapp"],
+
+        # SMB
+        AllowSMBComputeOut          = ["455", "Outbound", "Allow", "*",   "SMB",                "subnet/compute",            "subnet/netapp"],
 
         # Telegraf / Grafana
         AllowTelegrafOut            = ["460", "Outbound", "Allow", "Tcp", "Telegraf",           "asg/asg-telegraf",          "asg/asg-grafana"],
