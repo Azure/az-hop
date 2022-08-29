3be22669cbfeb5d3db357aa358c013466be6c61e
Matt Chan <v-matchan@microsoft.com>
2022-08-29 09:57:40 -0700

    change NSG dependency

--- tf/network_security_group.tf
+++ tf/network_security_group.tf
@@ -25,7 +25,14 @@ data "azurerm_application_security_group" "asg" {
 
 # Read subnets data so we can dynamically retrieve all CIDR for the NSG rules
 data "azurerm_subnet" "subnets" {
-  depends_on   = [time_sleep.wait_forsubnets] 
+#  depends_on   = [time_sleep.wait_forsubnets] 
+  depends_on   = [azurerm_subnet.ad, 
+                  azurerm_subnet.frontend, 
+                  azurerm_subnet.admin, 
+                  azurerm_subnet.netapp, 
+                  azurerm_subnet.compute, 
+                  azurerm_subnet.bastion, 
+                  azurerm_subnet.gateway] 
   for_each = local.subnets
   name                 = try(local.configuration_yml["network"]["vnet"]["subnets"][each.key]["name"], each.value)
   resource_group_name  = local.create_vnet ? (local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name) : data.azurerm_virtual_network.azhop[0].resource_group_name
