bb7ecf592360052f08cd402e542e5ad30d810567
vgamayunov <74012931+vgamayunov@users.noreply.github.com>
2022-09-23 14:57:43 +0100

    remove libnvidia-container-tools (#1099)
    * remove libnvidia-container-tools from CentOS and Ubuntu images

--- packer/scripts/centos/slurm.sh
+++ packer/scripts/centos/slurm.sh
@@ -93,12 +93,12 @@ arch=$(uname -m)
 rpm -q enroot || yum install -y https://github.com/NVIDIA/enroot/releases/download/v${ENROOT_VERSION}/enroot-${ENROOT_VERSION_FULL}.el7.${arch}.rpm
 rpm -q enroot+caps || yum install -y https://github.com/NVIDIA/enroot/releases/download/v${ENROOT_VERSION}/enroot+caps-${ENROOT_VERSION_FULL}.el7.${arch}.rpm
 
-# Install NVIDIA container support
-DIST=$(. /etc/os-release; echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/libnvidia-container/$DIST/libnvidia-container.repo > /etc/yum.repos.d/libnvidia-container.repo
+# # Install NVIDIA container support
+# DIST=$(. /etc/os-release; echo $ID$VERSION_ID)
+# curl -s -L https://nvidia.github.io/libnvidia-container/$DIST/libnvidia-container.repo > /etc/yum.repos.d/libnvidia-container.repo
 
-yum -y makecache
-yum -y install libnvidia-container-tools
+# yum -y makecache
+# yum -y install libnvidia-container-tools
 
 # Add kernel boot parameters
 grep user.max_user_namespaces /etc/sysctl.conf || echo 'user.max_user_namespaces = 1417997' >> /etc/sysctl.conf
