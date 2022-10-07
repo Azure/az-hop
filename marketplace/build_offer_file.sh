7271375f7e43cfe50f472125cd11998b336f5750
Xavier Pillons <xpillons@microsoft.com>
2022-10-07 17:45:01 +0200

    include azhop in the offer name

--- marketplace/build_offer_file.sh
+++ marketplace/build_offer_file.sh
@@ -7,7 +7,7 @@ os=$2
 
 SA_ACCOUNT=azhop
 
-os_disk_id=$(az disk list -g azhop_build_images --query "[?name=='azhop-${offer}-${os}'].id" -o tsv)
+os_disk_id=$(az disk list -g azhop_build_images --query "[?name=='${offer}-${os}'].id" -o tsv)
 version=$(az disk show --id $os_disk_id --query "tags.Version" -o tsv)
 
 start=$(date -u -d "-10 minutes" '+%Y-%m-%dT%H:%MZ')
@@ -16,7 +16,7 @@ expiry=$(date -u -d "+4 years" '+%Y-%m-%dT%H:%MZ') # 4 years expiration SAS
 icon_sas=$(az storage container generate-sas --account-name $SA_ACCOUNT --name icons --permissions rl --start $start --expiry $expiry --output tsv)
 image_sas=$(az storage container generate-sas --account-name $SA_ACCOUNT --name images --permissions rl --start $start --expiry $expiry --output tsv)
 
-sasurl=$(echo "https://$SA_ACCOUNT.blob.core.windows.net/images/azhop-${offer}-${os}-v${version}.vhd?${image_sas}")
+sasurl=$(echo "https://$SA_ACCOUNT.blob.core.windows.net/images/${offer}-${os}-v${version}.vhd?${image_sas}")
 
 cat <<EOF >./offers/temp.json
 {
@@ -29,7 +29,7 @@ EOF
 
 # For this plan
 # Add the new version to the existing versions
-jq '(.definition.plans[] | select(.planId==$plan)."microsoft-azure-virtualmachines.vmImages") += $version ' --arg plan ${os} --argjson version "$(jq '.' ./offers/temp.json)" ./offers/azhop-${offer}-${os}.json > ./offers/azhop-${offer}-${os}-final.json
+jq '(.definition.plans[] | select(.planId==$plan)."microsoft-azure-virtualmachines.vmImages") += $version ' --arg plan ${os} --argjson version "$(jq '.' ./offers/temp.json)" ./offers/${offer}-${os}.json > ./offers/${offer}-${os}-final.json
 
 #fname=./offers/${offer}-${os}-final.json
 
