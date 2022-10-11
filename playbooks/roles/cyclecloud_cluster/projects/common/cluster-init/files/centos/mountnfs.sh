4cf149d81574dfc60ad06ede01d8c1ecf04e2d13
Xavier Pillons <xpillons@microsoft.com>
2022-10-11 16:53:24 +0200

    NFS mount options as parameters

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/files/centos/mountnfs.sh
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/files/centos/mountnfs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 packages="nfs-utils"
-homedir_mountpoint=$1
-nfs_home_ip=$2
-nfs_home_path=$3
+homedir_mountpoint="$1"
+nfs_home_ip="$2"
+nfs_home_path="$3"
+nfs_home_opts="$4"
 
 if ! rpm -q $packages; then
   echo "Installing packages $packages"
@@ -11,6 +12,6 @@ fi
 
 mkdir $homedir_mountpoint
 echo "mount $nfs_home_ip:/$nfs_home_path $homedir_mountpoint"
-mount -t nfs -o rw,hard,rsize=262144,wsize=262144,vers=3,tcp $nfs_home_ip:/$nfs_home_path $homedir_mountpoint || exit 1
+mount -t nfs -o $nfs_home_opts $nfs_home_ip:/$nfs_home_path $homedir_mountpoint || exit 1
 
 setsebool -P use_nfs_home_dirs 1 || true
