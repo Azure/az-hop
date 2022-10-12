6899ab17d18aca76ddd6cdeae056d0d3907fa26a
Xavier Pillons <xpillons@microsoft.com>
2022-10-12 14:40:38 +0200

    use a parameter file to deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,15 +4,16 @@ set -o pipefail
 rg=$1
 location=${2:-westeurope}
 
+parameter_file=$rg-$location.params.json
+
 if [ "$rg" == "" ]; then
     echo "Usage: $0 <resource group> <location>"
     exit 1
 fi
 
+# When applied on an existing deployment, don't create new passwords
 adminuser=hpcadmin
-winpassword=$(pwgen -s 12 -1)
 slurmadmin=sqladmin
-slurmactpassword=$(pwgen -s 12 -1)
 
 if [[ ! -f "${adminuser}_id_rsa" ]]; then
     ssh-keygen -f "${adminuser}_id_rsa"  -N ''
@@ -26,17 +27,52 @@ if [[ $(yq .queue_manager build.yml) == "slurm" && $(yq .slurm.accounting_enable
     slurmAdminPasswordArg="slurmAccountingAdminPassword=$slurmactpassword"
 fi
 
+if [ ! -e $parameter_file ]; then
+echo "Parameter file doesn't exists, create it"
+winpassword=$(pwgen -s 12 -1)
+slurmactpassword=$(pwgen -s 12 -1)
+
+cat <<EOF >$parameter_file
+{
+    "\$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "adminUser": {
+            "value": "$adminuser"
+        },
+        "homedirMountpoint": {
+            "value": "/anfhome"
+        },
+        "adminSshPublicKey": {
+            "value": "$(<${adminuser}_id_rsa.pub)"
+        },
+        "adminSshPrivateKey": {
+            "value": "$(<${adminuser}_id_rsa)"
+        },
+        "adminPassword": {
+            "value": "$winpassword"
+        },
+        "location": {
+            "value": "$location"
+        },
+        "slurmAccountingAdminUser": {
+            "value": "$slurmadmin"
+        },
+        "slurmAccountingAdminPassword": {
+            "value": "$slurmactpassword"
+        }
+    }
+}
+EOF
+fi
+
 ./build.sh
+
 az group create --location $location --name $rg
 az deployment group create \
     --resource-group $rg \
     --template-file azureDeploy.bicep \
-    --parameters \
-        adminUser=$adminuser \
-        adminPassword="$winpassword" \
-        adminSshPublicKey="$(<${adminuser}_id_rsa.pub)" \
-        adminSshPrivateKey="$(<${adminuser}_id_rsa)" \
-        $slurmAdminUserArg $slurmAdminPasswordArg \
+    --parameters @$parameter_file \
     | tee deploy.log
 
 jq '.properties.outputs | to_entries | map({(.key): .value.value}) | add' <deploy.log | yq -P | tee outputs.yml
