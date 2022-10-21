8d09d3cfc9f9f02d0107742f1c5ec3cdd2b9bf4b
Xavier Pillons <xpillons@microsoft.com>
2022-10-21 15:26:11 +0200

    inject user principal ID in the keyvault_readers

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,6 +12,36 @@ if [ "$rg" == "" ]; then
     exit 1
 fi
 
+# Retrieve the user principal ID to grant access to the KV
+
+user_type=$(az account show --query user.type -o tsv)
+if [ ${user_type} == "user" ]; then
+  logged_user_objectId=$(az ad signed-in-user show --query id -o tsv)
+else
+  export clientId=$(az account show --query user.name -o tsv)
+  case "${clientId}" in
+      "systemAssignedIdentity")
+          mds=$(curl -s --noproxy "*" -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2019-08-15")
+          vmname=$(echo $mds | jq -r '.compute.name')
+          rgname=$(echo $mds | jq -r '.compute.resourceGroupName')
+          echo " - logged in Azure with System Assigned Identity from ${vmname}/${rgname}"
+          logged_user_objectId=$(az resource list -n $vmname -g $rgname --query [*].identity.principalId --out tsv)
+          if [ "$logged_user_objectId" == "" ]; then
+            logged_user_objectId=$(az resource list -n $vmname -g $rgname --query [*].identity.userAssignedIdentities.*.principalId --out tsv)
+          fi
+          ;;
+      "userAssignedIdentity")
+          echo "userAssignedIdentity not supported; please use a systemAssignedIdentity or a Service Principal Name instead"
+          exit 1
+          ;;
+      *)
+          logged_user_objectId=$(az ad sp show --id ${clientId} --query id -o tsv)
+          ;;
+  esac
+fi
+
+sed -i "s/^keyvault_readers: \[/keyvault_readers: \[$logged_user_objectId,/g" build.yml
+
 # When applied on an existing deployment, don't create new passwords
 adminuser=hpcadmin
 slurmadmin=sqladmin
