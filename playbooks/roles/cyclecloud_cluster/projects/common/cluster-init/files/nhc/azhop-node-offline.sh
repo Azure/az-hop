0c8c33c2811373bdf52a5f426edb575554aa346e
vgamayunov <74012931+vgamayunov@users.noreply.github.com>
2022-11-03 17:23:22 +0000

    improved nhc error handling (#1158)
    * log nhc error message with jetpack
    * shutdown immediately if nhc fails on node startup
    * delay node shutdown to allow clean job exit
    
    Co-authored-by: Xavier Pillons <xpillons@microsoft.com>

---
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/files/nhc/azhop-node-offline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This code is executed when NHC detects an error
+# Parameters:
+#    1: SLURM node name
+#    2+: Error message
+# Logs following information in CC log and requests CC to terminate the node:
+#    Hostname
+#    Physical host name
+#    NHC error message
+
+echo "`date '+%Y%m%d %H:%M:%S'` $0 $*"
+
+HOSTNAME="$1"
+shift
+NOTE="$*"
+
+PHYSICAL_HOST=$(strings /var/lib/hyperv/.kvp_pool_3 | grep -A1 PhysicalHostName | head -n 2 | tail -1)
+JETPACK=/opt/cycle/jetpack/bin/jetpack
+
+echo "$0:  ERROR : Node Health Checks failed on $HOSTNAME - $PHYSICAL_HOST - $NOTE"
+$JETPACK log "ERROR : Node Health Checks failed - $(hostname) - $PHYSICAL_HOST - $NOTE" --level error
+/usr/libexec/nhc/node-mark-offline $HOSTNAME $NOTE
+echo "$JETPACK shutdown --unhealthy" | at now +2 minutes
+# To keep the VM online for debugging, replace the line above with the following:
+#  $JETPACK keepalive forever
