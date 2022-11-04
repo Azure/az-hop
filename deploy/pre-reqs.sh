2fe72a440f3c2f1d631eb7c22a5e4ae80edab8ee
Xavier Pillons <xpillons@microsoft.com>
2022-11-04 17:57:11 +0100

    add OnDemand FQDN

--- deploy/pre-reqs.sh
+++ deploy/pre-reqs.sh
@@ -7,4 +7,6 @@ set -e
 echo "Installing yq...."
 VERSION=v4.25.3
 BINARY=yq_linux_amd64
-wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O ${HOME}/bin/yq && chmod +x ${HOME}/bin/yq
+mkdir -p ${HOME}/bin
+wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O ${HOME}/bin/yq 
+chmod +x ${HOME}/bin/yq
