c1e2e01b5f3b6f79623f7a023a4e8ad59c0085bf
Xavier Pillons <xpillons@microsoft.com>
2022-12-14 14:52:17 +0100

    use enroot temp and jobid

--- playbooks/roles/ood-applications/files/bc_jupyter/template/get_container.sh
+++ playbooks/roles/ood-applications/files/bc_jupyter/template/get_container.sh
@@ -1,17 +1,36 @@
 #!/usr/bin/env bash
+set -e
 echo "Getting container..."
 echo "TTT - $(date)"
 container_image=$1
 container_name=$2
 
-# TODO: add docker:// if no prefix is provided
-enroot import docker://$container_image
+echo "container_image=$container_image"
+echo "container_name=$container_name"
+# add the default docker:// if no prefix is provided
+if [ "$container_image" != *"://"* ]; then
+    container_image="docker://$container_image"
+    echo "container_image=$container_image"
+fi
 
-# replace [/:] by +
-container_image=${container_image//\//+}
-container_image=${container_image//:/+}
+echo "enroot import"
+# remove any prefix :// and replace chars in (/:) by +
+container_squashfs_path=${container_image#*://}
+container_squashfs_path=${container_squashfs_path//\//+}
+container_squashfs_path=${container_squashfs_path//:/+}
 
-# TODO : place the sqsh file in the enroot temporary configured
-enroot create --name $container_name $container_image.sqsh
+# get the enroot temp path to store squashfs files
+ENROOT_TEMP_PATH=$(grep ENROOT_TEMP_PATH /etc/enroot/enroot.conf | cut -d' ' -f2)
+ENROOT_TEMP_PATH=${ENROOT_TEMP_PATH:-/tmp}
+echo "ENROOT_TEMP_PATH=$ENROOT_TEMP_PATH"
+container_squashfs_path="$ENROOT_TEMP_PATH/$container_squashfs_path.sqsh"
+echo "container_squashfs_path=$container_squashfs_path"
 
-# TODO: remove sqsh file
+# import the container and save the sqsh file in the enroot temporary path configured in /etc/enroot/enroot.conf
+enroot import --output $container_squashfs_path $container_image
+
+echo "enroot create container"
+enroot create --name $container_name $container_squashfs_path
+
+echo "remove squashfs file $container_squashfs_path"
+rm -f $container_squashfs_path
