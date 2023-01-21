267ed45e03cf7485125120573d4d19acb04caf8d
Brian Santacruz <bsantacruz@microsoft.com>
2023-01-21 10:26:39 -0800

    Guacamole and Lustre SMI
    Pushing changes to Guacamole and Lustre VMs. Cleaning duplicated files.

--- tf/ccportal.tf
+++ tf/ccportal.tf
@@ -30,19 +30,16 @@ resource "azurerm_linux_virtual_machine" "ccportal" {
     storage_account_type = "Standard_LRS"
   }
 
-  /*USER MANAGE IDENTITY CONFIG
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [ azurerm_user_assigned_identity.ccportal.id ]
-  }
-  */
+  # USER MANAGE IDENTITY CONFIG
+  # identity {
+  # type         = "UserAssigned"
+  # identity_ids = [ azurerm_user_assigned_identity.ccportal.id ]
+  #}
 
-
-  /*SYSTEM MANAGE IDENTITY CONFIG*/
+  #SYSTEM MANAGE IDENTITY CONFIG
   identity {
     type         = "SystemAssigned"
   }
-  /**/
 
   dynamic "source_image_reference" {
     for_each = local.use_linux_image_id ? [] : [1]
@@ -95,12 +92,12 @@ data "azurerm_role_definition" "reader" {
   name = "Reader"
 }
 
-/*USER MANAGE IDENTITY CONFIG
-resource "azurerm_user_assigned_identity" "ccportal" {
-  location            = local.create_rg ? azurerm_resource_group.rg[0].location : data.azurerm_resource_group.rg[0].location
-  resource_group_name = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
-  name = "ccportal"
-}*/
+#  USER MANAGE IDENTITY CONFIG
+#  resource "azurerm_user_assigned_identity" "ccportal" {
+#  location            = local.create_rg ? azurerm_resource_group.rg[0].location : data.azurerm_resource_group.rg[0].location
+#  resource_group_name = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
+#  name = "ccportal"
+#}
 
 # resource "random_uuid" "role" {
 # }
@@ -157,35 +154,34 @@ resource "azurerm_user_assigned_identity" "ccportal" {
 #   }
 # }
 
-/*USER MANAGE IDENTITY CONFIG
+# USER MANAGE IDENTITY CONFIG
 # Grant Contributor access to Cycle in the az-hop resource group
-resource "azurerm_role_assignment" "ccportal_rg" {
+# resource "azurerm_role_assignment" "ccportal_rg" {
 #  name               = azurerm_user_assigned_identity.ccportal.principal_id
-  scope              = local.create_rg ? azurerm_resource_group.rg[0].id : data.azurerm_resource_group.rg[0].id
-  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.contributor.id}"
-  principal_id       = azurerm_user_assigned_identity.ccportal.principal_id
-}*/
+#  scope              = local.create_rg ? azurerm_resource_group.rg[0].id : data.azurerm_resource_group.rg[0].id
+#  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.contributor.id}"
+#  principal_id       = azurerm_user_assigned_identity.ccportal.principal_id
+#}
 
 
-/*SYSTEM MANAGE IDENTITY CONFIG*/
+#SYSTEM MANAGE IDENTITY CONFIG
 # Grant Contributor access to Cycle in the az-hop resource group
 resource "azurerm_role_assignment" "ccportal_rg" {
   scope              = local.create_rg ? azurerm_resource_group.rg[0].id : data.azurerm_resource_group.rg[0].id
   role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.contributor.id}"
   principal_id       = azurerm_linux_virtual_machine.ccportal.identity[0].principal_id
-}/**/
+}
 
-/*USER MANAGE IDENTITY CONFIG
+#USER MANAGE IDENTITY CONFIG
 # Grant Subscription Reader access to Cycle
 resource "azurerm_role_assignment" "ccportal_sub_reader" {
   scope              = "${data.azurerm_subscription.primary.id}"
   role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.reader.id}"
   principal_id       = azurerm_user_assigned_identity.ccportal.principal_id
 }
-*/
 
 
-/*SYSTEM MANAGE IDENTITY CONFIG*/
+#SYSTEM MANAGE IDENTITY CONFIG
 # Grant Subscription Reader access to Cycle
 resource "azurerm_role_assignment" "ccportal_sub_reader" {
   scope              = "${data.azurerm_subscription.primary.id}"
--- tf/guacamole.tf
+++ tf/guacamole.tf
@@ -53,10 +53,20 @@ resource "azurerm_linux_virtual_machine" "guacamole" {
     }
   }
 
+  /*USER MANAGE IDENTITY CONFIG
   identity {
     type         = "UserAssigned"
     identity_ids = [ azurerm_user_assigned_identity.guacamole[0].id ]
   }
+  */
+
+      /*SYSTEM MANAGE IDENTITY CONFIG*/
+  identity {
+    type         = "SystemAssigned"
+  }
+  /**/
+
+
   lifecycle {
     ignore_changes = [
       tags
@@ -64,13 +74,15 @@ resource "azurerm_linux_virtual_machine" "guacamole" {
   }
 }
 
+/*USER MANAGE IDENTITY CONFIG
 resource "azurerm_user_assigned_identity" "guacamole" {
   count               = local.enable_remote_winviz ? 1 : 0
   location            = local.create_rg ? azurerm_resource_group.rg[0].location : data.azurerm_resource_group.rg[0].location
   resource_group_name = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
 
   name = "guacamole"
-}
+}*/
+
 # Grant read access to the Keyvault for the guacamole identity
 resource "azurerm_key_vault_access_policy" "guacamole" {
   count               = local.enable_remote_winviz ? 1 : 0
