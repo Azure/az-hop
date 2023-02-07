e22822e6be6148cbeed7dc4541d0848d7876f42a
Paul Edwards <paul.edwards@microsoft.com>
2023-02-07 09:50:03 +0000

    Updated Lustre client

--- packer/scripts/almalinux/lustreclient.sh
+++ packer/scripts/almalinux/lustreclient.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-lustre_version=${1-2.12.5}
-lustre_dir="lustre-$lustre_version"
 
-cat << EOF >/etc/yum.repos.d/LustrePack.repo
-#[lustreserver]
-#name=lustreserver
-#baseurl=https://downloads.whamcloud.com/public/lustre/${lustre_dir}/el8/patchless-ldiskfs-server/
-#enabled=0
-#gpgcheck=0
+set -ex
+   
+DISTRIB_CODENAME=el8
+REPO_PATH=/etc/yum.repos.d/amlfs.repo
+   
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+   
+echo -e "[amlfs]" > ${REPO_PATH}
+echo -e "name=Azure Lustre Packages" >> ${REPO_PATH}
+echo -e "baseurl=https://packages.microsoft.com/yumrepos/amlfs-${DISTRIB_CODENAME}" >> ${REPO_PATH}
+echo -e "enabled=1" >> ${REPO_PATH}
+echo -e "gpgcheck=1" >> ${REPO_PATH}
+echo -e "gpgkey=https://packages.microsoft.com/keys/microsoft.asc" >> ${REPO_PATH}
 
-[e2fs]
-name=e2fs
-baseurl=https://downloads.whamcloud.com/public/e2fsprogs/latest/el8/
-enabled=1
-gpgcheck=0
-
-[lustreclient]
-name=lustreclient
-baseurl=https://downloads.whamcloud.com/public/lustre/${lustre_dir}/el8/client/
-enabled=1
-gpgcheck=0
-EOF
-
-dnf -y install kmod-lustre-client lustre-client || exit 1
-weak-modules --add-kernel $(uname -r) || exit 1
+dnf install amlfs-lustre-client-2.15.1_24_gbaa21ca-$(uname -r | sed -e "s/\.$(uname -p)$//" | sed -re 's/[-_]/\./g')-1
