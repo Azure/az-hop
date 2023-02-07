489e1ffb8976ed19be0c8e9a1eaa0f3bd30fa91c
Paul Edwards <paul.edwards@microsoft.com>
2023-02-07 10:24:42 +0000

    missing "-y" in "dnf install"

--- packer/scripts/almalinux/lustreclient.sh
+++ packer/scripts/almalinux/lustreclient.sh
@@ -14,4 +14,4 @@ echo -e "enabled=1" >> ${REPO_PATH}
 echo -e "gpgcheck=1" >> ${REPO_PATH}
 echo -e "gpgkey=https://packages.microsoft.com/keys/microsoft.asc" >> ${REPO_PATH}
 
-dnf install amlfs-lustre-client-2.15.1_24_gbaa21ca-$(uname -r | sed -e "s/\.$(uname -p)$//" | sed -re 's/[-_]/\./g')-1
+dnf install -y amlfs-lustre-client-2.15.1_24_gbaa21ca-$(uname -r | sed -e "s/\.$(uname -p)$//" | sed -re 's/[-_]/\./g')-1
