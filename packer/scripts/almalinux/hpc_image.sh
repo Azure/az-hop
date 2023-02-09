1006374b43a7ee2871c83dc1d6fdfcf290c4751a
Paul Edwards <paul.edwards@microsoft.com>
2023-02-09 12:30:49 +0000

    testing 8.7

---
+++ packer/scripts/almalinux/hpc_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+yum install -y git
+
+chmod 777 /mnt/resource
+cd /mnt/resource
+
+git clone https://github.com/Azure/azhpc-images # -b centos-hpc-20220112
+
+cd ./azhpc-images/alma/alma-8.x/alma-8.6-hpc
+
+./install.sh
+
+#cd /mnt/resource/azhpc-images/tests/run-tests.sh --mofed-lts false
