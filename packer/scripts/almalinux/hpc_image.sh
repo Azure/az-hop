73547e8ea126f0c6eb9fff80fd4f3e5d5af5d367
Paul Edwards <paul.edwards@microsoft.com>
2023-02-09 15:35:06 +0000

    changed source for azhpc-images

--- packer/scripts/almalinux/hpc_image.sh
+++ packer/scripts/almalinux/hpc_image.sh
@@ -8,7 +8,7 @@ mkdir -p /mnt/resource
 chmod 777 /mnt/resource
 cd /mnt/resource
 
-git clone https://github.com/Azure/azhpc-images # -b centos-hpc-20220112
+git clone https://github.com/edwardsp/azhpc-images # -b centos-hpc-20220112
 
 cd ./azhpc-images/alma/alma-8.x/alma-8.6-hpc
 
