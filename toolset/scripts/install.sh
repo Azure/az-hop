c12ddb90239aa6a99a72d520eebfbb335e47f316
Leopold Talirz <a-ctalirz@microsoft.com>
2023-02-19 15:24:01 +0000

    generic: add schema for config.yml
    Add JSON schema for validating config.yml
    
    The confing.yml is automatically validated against the schema when running `build.sh`.
    A "mode key" is added to the `config.tpl.yml`, which points to the schema and provides
    automatic validation in VSCode with the "YAML" extension by RedHat.

--- toolset/scripts/install.sh
+++ toolset/scripts/install.sh
@@ -103,6 +103,13 @@ VERSION=v4.25.3
 BINARY=yq_linux_amd64
 wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/bin/yq && chmod +x /usr/bin/yq
 
+#
+# Install jsonschema
+#
+echo "Installing jsonschema...."
+VERSION=4.17.3
+pip3 install jsonschema==$VERSION
+
 # Clean-up
 rm -f /tmp/*.zip && rm -f /tmp/*.gz && \
 
