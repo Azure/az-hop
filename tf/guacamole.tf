9f06437cd6e438a6e84bddcfd3a4c9f7b153ca6d
egmsft <egallardo@microsoft.com>
2023-02-22 12:58:33 -0600

    only try to create the agent for the guacamole VM if remote_winviz is enabled

--- tf/guacamole.tf
+++ tf/guacamole.tf
@@ -81,9 +81,7 @@ resource "azurerm_network_interface_application_security_group_association" "gua
 }
 
 resource "azurerm_virtual_machine_extension" "AzureMonitorLinuxAgent_guacamole" {
-  # depends_on = [
-  #   azurerm_linux_virtual_machine.guacamole
-  # ]
+  count                      = local.enable_remote_winviz ? 1 : 0
   name                       = "AzureMonitorLinuxAgent"
   virtual_machine_id         = azurerm_linux_virtual_machine.guacamole[0].id
   publisher                  = "Microsoft.Azure.Monitor"
