8029b2524b634c870d326ccf28251f8dd6af4fff
Victor Gamayunov <vgamayunov@microsoft.com>
2023-03-01 14:13:32 +0000

    amlfs lustre client

--- packer/scripts/ubuntu/lustreclient.sh
+++ packer/scripts/ubuntu/lustreclient.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-lustre_version=${1:-b2_14}
+LUSTRE_VERSION=2.15.1-24-gbaa21ca
+
+source /etc/lsb-release
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" > /etc/apt/sources.list.d/amlfs.list
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 
 apt-get update
-apt-get install -y libyaml-dev
-apt-get install -y module-assistant libreadline-dev libselinux-dev libsnmp-dev mpi-default-dev
-
-echo "Downloading Lustre source code..."
-cd /mnt
-git clone git://git.whamcloud.com/fs/lustre-release.git
-cd lustre-release
-
-echo "Building Lustre client..."
-git checkout $lustre_version
-bash autogen.sh
-./configure --with-o2ib=no --disable-server
-make -j debs
-
-echo "Installing Lustre client packages..."
-dpkg -i debs/lustre-client-modules*deb 
-dpkg -i debs/lustre-client-utils*deb
-
+apt-get install -y amlfs-lustre-client-${LUSTRE_VERSION}=$(uname -r)
