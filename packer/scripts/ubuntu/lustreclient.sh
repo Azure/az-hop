f3f24656b698b7e536963b57e2e8e53cbe871dfb
Victor Gamayunov <vgamayunov@microsoft.com>
2023-03-01 17:39:03 +0000

    cosmetic change

--- packer/scripts/ubuntu/lustreclient.sh
+++ packer/scripts/ubuntu/lustreclient.sh
@@ -2,9 +2,8 @@
 LUSTRE_VERSION=2.15.1-24-gbaa21ca
 
 source /etc/lsb-release
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" > /etc/apt/sources.list.d/amlfs.list
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg
 
 apt-get update
 apt-get install -y amlfs-lustre-client-${LUSTRE_VERSION}=$(uname -r)
