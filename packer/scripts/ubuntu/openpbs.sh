37d6b1b7e8a69b48e3b1009b41a7855aec811e36
Victor Gamayunov <vgamayunov@microsoft.com>
2023-03-01 11:38:50 +0000

    add ubuntu-20.04 support for pbs

--- packer/scripts/ubuntu/openpbs.sh
+++ packer/scripts/ubuntu/openpbs.sh
@@ -6,15 +6,32 @@ echo " *                                                                        
 echo " ********************************************************************************** "
 
 cd /mnt
-wget -q https://github.com/openpbs/openpbs/releases/download/v19.1.1/pbspro-19.1.1.tar.gz -O pbspro-19.1.1.tar.gz
-tar -xzf pbspro-19.1.1.tar.gz
-cd pbspro-19.1.1/
 
 apt-get install -y gcc make libtool libhwloc-dev libx11-dev libxt-dev libedit-dev libical-dev ncurses-dev perl postgresql-server-dev-all postgresql-contrib python-dev tcl-dev tk-dev swig libexpat-dev libssl-dev libxext-dev libxft-dev autoconf automake
-./autogen.sh
-./configure --prefix=/opt/pbs PYTHON=/usr/bin/python2.7
-make
-make install
+
+wget -q https://github.com/openpbs/openpbs/releases/download/v19.1.1/pbspro-19.1.1.tar.gz -O pbspro-19.1.1.tar.gz
+tar -xzf pbspro-19.1.1.tar.gz
+
+if [ "$(dpkg -s libhwloc-dev| grep Version| sed 's/^[^0-9]*\([0-9]\).*$/\1/')" == "2" ]; then
+    git clone https://github.com/open-mpi/hwloc.git -b v1.11
+    cd hwloc
+    ./autogen.sh
+    ./configure --enable-static --enable-embedded-mode
+    make
+    cd ..
+
+    cd pbspro-19.1.1/
+    ./autogen.sh
+    ./configure --prefix=/opt/pbs PYTHON=/usr/bin/python2.7 CFLAGS="-I$PWD/../hwloc/include" LDFLAGS="-L$PWD/../src"
+    make
+    make install
+else
+    cd pbspro-19.1.1/
+    ./autogen.sh
+    ./configure --prefix=/opt/pbs PYTHON=/usr/bin/python2.7
+    make
+    make install
+fi
 
 /opt/pbs/libexec/pbs_postinstall execution
 chmod 4755 /opt/pbs/sbin/pbs_iff /opt/pbs/sbin/pbs_rcp
