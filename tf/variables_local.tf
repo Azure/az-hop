d6abd2a98ac367c94386546fbe4c76e445e2f7af
Xavier Pillons <xpillons@microsoft.com>
2023-03-16 10:30:46 +0100

    fix empty alert_email

--- tf/variables_local.tf
+++ tf/variables_local.tf
@@ -67,11 +67,11 @@ locals {
     monitor = ( local.create_log_analytics_workspace || local.use_existing_ws ) ? true : false
     ama_install = try(local.configuration_yml["monitoring"]["install_agent"], true) && local.monitor ? true : false
 
-    alert_email = try(local.configuration_yml["alerting"]["admin_email"], null)
+    alert_email = try(local.configuration_yml["alerting"]["admin_email"], "admin.mail@contoso.com")
 
     #For alerting to be enabled - the analytics workspace needs to be created since log alerts are leveraged. 
     #We also need to ensure that we have an email to send alerts to.  
-    create_alerts = local.monitor && local.alert_email != null && local.alert_email != "admin.mail@contoso.com" && try(local.configuration_yml["alerting"]["enabled"], false) ? true : false
+    create_alerts = local.monitor && local.alert_email != "admin.mail@contoso.com" && try(local.configuration_yml["alerting"]["enabled"], false) ? true : false
     anf_vol_threshold = try(local.configuration_yml["anf"]["alert_threshold"], 80)  # default to 80% if not specified 
 
     # will be used with a KQL query that checks the free space percentage of local volumes
