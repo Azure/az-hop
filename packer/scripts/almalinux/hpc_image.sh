12370a9c0aa274fcd1c48120ba817a4406f6a8ad
Paul Edwards <paul.edwards@microsoft.com>
2023-03-28 08:55:15 +0100

    Switched branch in azhpc-images
    To avoid issues if futher commits are made on the master branch

--- packer/scripts/almalinux/hpc_image.sh
+++ packer/scripts/almalinux/hpc_image.sh
@@ -8,7 +8,7 @@ mkdir -p /mnt/resource
 chmod 777 /mnt/resource
 cd /mnt/resource
 
-git clone https://github.com/edwardsp/azhpc-images # -b centos-hpc-20220112
+git clone https://github.com/edwardsp/azhpc-images -b azhop_alma87
 
 cd ./azhpc-images/alma/alma-8.x/alma-8.7-hpc
 
