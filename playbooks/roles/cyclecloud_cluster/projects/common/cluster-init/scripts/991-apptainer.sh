3423bc93e60e23c8aac7c7ce587a42ebeba08392
Xavier Pillons <xpillons@microsoft.com>
2023-03-29 15:33:55 +0200

    Delete 991-apptainer.sh

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/991-apptainer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$script_dir/../files/azhop-helpers.sh"
-read_os
-
-$script_dir/../files/$os_release/init_apptainer.sh
