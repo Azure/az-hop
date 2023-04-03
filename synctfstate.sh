67b846d69d8b8191334429350fdb8f1126f09a18
Xavier Pillons <xpillons@microsoft.com>
2023-04-03 18:23:29 +0200

    add ASGs

--- synctfstate.sh
+++ synctfstate.sh
@@ -21,7 +21,7 @@ subscription_id=$(az account show -o json | jq -r '.id')
 kv_name=$(az keyvault list -g $resource_group --query "[].name" -o tsv)
 if [ -z $kv_name ]; then
     echo "Keyvault $kv_name not found in $resource_group"
-    exit 1
+    #exit 1
 fi
 suffix=${kv_name:2:${#kv_name}}
 echo $suffix
@@ -53,19 +53,40 @@ function import_state () {
         fi
         if [ "$resource_id" == "null" ]; then
             echo "Resource $resource_name not found"
-            exit 1
+        else
+            echo "Importing $tf_name from $resource_id"
+            terraform -chdir=$TF_FOLDER import $tf_name $resource_id
         fi
-
-        echo "Importing $tf_name from $resource_id"
-        terraform -chdir=$TF_FOLDER import $tf_name $resource_id
     fi
 }
 
 # Import Resource Group
 import_state azurerm_resource_group.rg[0] resourceGroups $resource_group
 
+# Import Network
+import_state azurerm_virtual_network.azhop[0] Microsoft.Network/virtualNetworks hpcvnet
+import_state azurerm_application_security_group.asg[\"asg-ssh\"] Microsoft.Network/applicationSecurityGroups asg-ssh
+import_state azurerm_application_security_group.asg[\"asg-rdp\"] Microsoft.Network/applicationSecurityGroups asg-rdp
+import_state azurerm_application_security_group.asg[\"asg-jumpbox\"] Microsoft.Network/applicationSecurityGroups asg-jumpbox
+import_state azurerm_application_security_group.asg[\"asg-ad\"] Microsoft.Network/applicationSecurityGroups asg-ad
+import_state azurerm_application_security_group.asg[\"asg-ad-client\"] Microsoft.Network/applicationSecurityGroups asg-ad-client
+import_state azurerm_application_security_group.asg[\"asg-lustre\"] Microsoft.Network/applicationSecurityGroups asg-lustre
+import_state azurerm_application_security_group.asg[\"asg-lustre-client\"] Microsoft.Network/applicationSecurityGroups asg-lustre-client
+import_state azurerm_application_security_group.asg[\"asg-pbs\"] Microsoft.Network/applicationSecurityGroups asg-pbs
+import_state azurerm_application_security_group.asg[\"asg-pbs-client\"] Microsoft.Network/applicationSecurityGroups asg-pbs-client
+import_state azurerm_application_security_group.asg[\"asg-cyclecloud\"] Microsoft.Network/applicationSecurityGroups asg-cyclecloud
+import_state azurerm_application_security_group.asg[\"asg-cyclecloud-client\"] Microsoft.Network/applicationSecurityGroups asg-cyclecloud-client
+import_state azurerm_application_security_group.asg[\"asg-nfs-client\"] Microsoft.Network/applicationSecurityGroups asg-nfs-client
+import_state azurerm_application_security_group.asg[\"asg-telegraf\"] Microsoft.Network/applicationSecurityGroups asg-telegraf
+import_state azurerm_application_security_group.asg[\"asg-grafana\"] Microsoft.Network/applicationSecurityGroups asg-grafana
+import_state azurerm_application_security_group.asg[\"asg-robinhood\"] Microsoft.Network/applicationSecurityGroups asg-robinhood
+import_state azurerm_application_security_group.asg[\"asg-ondemand\"] Microsoft.Network/applicationSecurityGroups asg-ondemand
+import_state azurerm_application_security_group.asg[\"asg-deployer\"] Microsoft.Network/applicationSecurityGroups asg-deployer
+import_state azurerm_application_security_group.asg[\"asg-guacamole\"] Microsoft.Network/applicationSecurityGroups asg-guacamole
+import_state azurerm_application_security_group.asg[\"asg-mariadb-client\"] Microsoft.Network/applicationSecurityGroups asg-mariadb-client
+
 # Import virtual machines
-import_state azurerm_linux_virtual_machine.ad Microsoft.Compute/virtualMachines ad
+import_state azurerm_linux_virtual_machine.ad[0] Microsoft.Compute/virtualMachines ad
 import_state azurerm_network_interface.ad-nic Microsoft.Network/networkInterfaces ad-nic
 
 import_state azurerm_linux_virtual_machine.jumpbox[0] Microsoft.Compute/virtualMachines jumpbox
@@ -83,9 +104,6 @@ import_state azurerm_network_interface.scheduler-nic Microsoft.Network/networkIn
 
 import_state azurerm_linux_virtual_machine.grafana Microsoft.Compute/virtualMachines grafana
 import_state azurerm_network_interface.grafana-nic Microsoft.Network/networkInterfaces grafana-nic
-
-# Import Network
-import_state azurerm_virtual_network.azhop[0] Microsoft.Network/virtualNetworks hpcvnet
 
 # Import Key Vault
 import_state azurerm_key_vault.azhop Microsoft.KeyVault/vaults kv$suffix
