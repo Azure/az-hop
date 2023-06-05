f3325b7de41653a5c68d90d72c7b37cf1f10b7dc
Xavier Pillons <xpillons@microsoft.com>
2023-06-05 19:19:31 +0200

    report the NHC error before dying

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/99-healthcheck.sh
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/99-healthcheck.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-/usr/sbin/nhc -d
+errormessage=$( /usr/sbin/nhc 2>&1)
+error=$?
 
-# In case of health check failure, shutdown the node
-if [ $? -eq 1 ]; then
+# In case of health check failure, shutdown the node by calling the script /usr/libexec/nhc/azhop-node-offline.sh
+if [ $error -eq 1 ]; then
+    /usr/libexec/nhc/azhop-node-offline.sh $(hostname) "$errormessage"
     JETPACK=/opt/cycle/jetpack/bin/jetpack
     $JETPACK shutdown --unhealthy
 fi
