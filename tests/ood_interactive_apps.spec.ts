527bc3238e0ad73d9f9aabeb25c72a791de5f370
Xavier Pillons <xpillons@microsoft.com>
2023-08-08 13:13:22 +0200

    cleanup and use monitoring menu to start CC

--- tests/ood_interactive_apps.spec.ts
+++ tests/ood_interactive_apps.spec.ts
@@ -1,35 +1,6 @@
 import {test, expect} from '@playwright/test';
 import config from './playwright.config';
 
-// test('Windows Desktop', async ({browser}) => {
-//     // create context with HTTP credentials
-//     const context = await browser.newContext();
-//     // Create a page
-//     const page = await context.newPage();
-//     await page.goto('/', { waitUntil: 'networkidle' });
-
-//     // Click text=Interactive Apps
-//     await page.click('text=Interactive Apps');
-
-//     // Click div[role="main"] >> text=Windows Desktop
-//     await page.click('div[role="main"] >> text=Windows Desktop');
-
-//     // Click input:has-text("Launch")
-//     await page.click('input:has-text("Launch")');
-
-//     // Click a:has-text("Delete")
-//     await page.click('a:has-text("Delete")');
-
-//     // Click text=Confirm
-//     await page.click('text=Confirm');
-
-//     await page.waitForLoadState('networkidle');
-//     await page.close()
-    
-//     // Close the browser
-//     await context.close();
-// });
-
 test('Linux Desktop', async ({ browser }) => {
     // create context with HTTP credentials
     const context = await browser.newContext();
@@ -48,8 +19,6 @@ test('Linux Desktop', async ({ browser }) => {
     await page.getByRole('button', { name: 'Launch' }).click();
 
     await page.locator('#batch_connect_sessions div').filter({ hasText: /Linux Desktop/ }).getByRole('button', { name: 'Delete Linux Desktop Session' }).click();
-    //await page.getByRole('link', { name: 'Delete' }).click();
-    //await page.getByRole('button', { name: 'Confirm' }).click();
 
     await page.waitForLoadState('networkidle');
     await page.close()
@@ -72,8 +41,6 @@ test('CodeServer', async ({ browser }) => {
     await page.getByRole('combobox', { name: 'Slot Type' }).selectOption('execute');
     await page.getByRole('button', { name: 'Launch' }).click();
     await page.locator('#batch_connect_sessions div').filter({ hasText: /Code Server/ }).getByRole('button', { name: 'Delete Code Server Session' }).click();
-//    await page.getByRole('link', { name: ' Delete' }).click();
-//    await page.getByRole('button', { name: 'Confirm' }).click();
 
     await page.waitForLoadState('networkidle');
     await page.close()
