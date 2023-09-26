44999b1675fa3788f811093694ed07f4cae230c0
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2023-09-26 12:36:32 +0200

    upgrade amlfs client

--- packer/scripts/ubuntu/lustreclient.sh
+++ packer/scripts/ubuntu/lustreclient.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LUSTRE_VERSION=2.15.1-24-gbaa21ca
+LUSTRE_VERSION=2.15.1-29-gbae0abe
 
 source /etc/lsb-release
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
