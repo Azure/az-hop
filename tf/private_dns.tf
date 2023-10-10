0eb65aa2f8d74d3893780af21b6562dc16e8df65
Xavier Pillons <xpillons@microsoft.com>
2023-10-10 18:15:54 +0200

    do not deploy Private DNS is not needed (#1729)

--- tf/private_dns.tf
+++ tf/private_dns.tf
@@ -1,13 +1,15 @@
 # https://www.techopedia.com/2/31981/networking/networking-hardware/dismissing-the-myth-that-active-directory-requires-microsoft-dns
 resource "azurerm_private_dns_zone" "azhop_private_dns" {
+  count               = local.create_dns_records? 1 : 0
   name                = local.domain_name
   resource_group_name = local.create_rg ? azurerm_resource_group.rg[0].name : data.azurerm_resource_group.rg[0].name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "azhop_dns_link" {
+  count                 = local.create_dns_records? 1 : 0
   name                  = "az-hop"
-  resource_group_name   = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  private_dns_zone_name = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name   = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.azhop_private_dns[0].name
   virtual_network_id    = local.create_vnet ? azurerm_virtual_network.azhop[0].id : data.azurerm_virtual_network.azhop[0].id
   registration_enabled  = false
 }
@@ -16,8 +18,8 @@ resource "azurerm_private_dns_zone_virtual_network_link" "azhop_dns_link" {
 resource "azurerm_private_dns_a_record" "ad" {
   count               = local.create_dns_records? 1 : 0
   name                = values(local.domain_controlers)[0]
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   records             = local.create_dns_records? [local.domain_controller_ips[0]] : []
 }
@@ -25,8 +27,8 @@ resource "azurerm_private_dns_a_record" "ad" {
 resource "azurerm_private_dns_a_record" "ad2" {
   count               = local.ad_ha ? 1 : 0
   name                = try(values(local.domain_controlers)[1], local.ad2_name)
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   records             = local.create_dns_records? [try(local.domain_controller_ips[1], local.domain_controller_ips[0])] : []
 }
@@ -35,8 +37,8 @@ resource "azurerm_private_dns_a_record" "ad2" {
 resource "azurerm_private_dns_srv_record" "ldap_tcp" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_ldap._tcp"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
 
   dynamic "record" {
@@ -52,8 +54,8 @@ resource "azurerm_private_dns_srv_record" "ldap_tcp" {
 resource "azurerm_private_dns_srv_record" "kpasswd_tcp" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_kpasswd._tcp"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -68,8 +70,8 @@ resource "azurerm_private_dns_srv_record" "kpasswd_tcp" {
 resource "azurerm_private_dns_srv_record" "kerberos_tcp" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_kerberos._tcp"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -84,8 +86,8 @@ resource "azurerm_private_dns_srv_record" "kerberos_tcp" {
 resource "azurerm_private_dns_srv_record" "gc_tcp" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_gc._tcp"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -100,8 +102,8 @@ resource "azurerm_private_dns_srv_record" "gc_tcp" {
 resource "azurerm_private_dns_srv_record" "kerberos_udp" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_kerberos._udp"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -116,8 +118,8 @@ resource "azurerm_private_dns_srv_record" "kerberos_udp" {
 resource "azurerm_private_dns_srv_record" "kpasswd_udp" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_kpasswd._udp"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -134,8 +136,8 @@ resource "azurerm_private_dns_srv_record" "kpasswd_udp" {
 resource "azurerm_private_dns_srv_record" "ldap_tcpdc_msdcs" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_ldap._tcp.dc._msdcs"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -150,8 +152,8 @@ resource "azurerm_private_dns_srv_record" "ldap_tcpdc_msdcs" {
 resource "azurerm_private_dns_srv_record" "kerberos_tcpdc_msdcs" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_kerberos._tcp.dc._msdcs"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -166,8 +168,8 @@ resource "azurerm_private_dns_srv_record" "kerberos_tcpdc_msdcs" {
 resource "azurerm_private_dns_srv_record" "ldap_tcp_gc_msdcs" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_ldap._tcp.gc._msdcs"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -182,8 +184,8 @@ resource "azurerm_private_dns_srv_record" "ldap_tcp_gc_msdcs" {
 resource "azurerm_private_dns_srv_record" "ldap_tcppdc_msdcs" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_ldap._tcp.pdc._msdcs"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   record {
     priority = 0
@@ -195,8 +197,8 @@ resource "azurerm_private_dns_srv_record" "ldap_tcppdc_msdcs" {
 resource "azurerm_private_dns_srv_record" "ldapdefault-first-site-name_sitesdc_msdcs" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_ldap.default-first-site-name._sites.dc._msdcs"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -211,8 +213,8 @@ resource "azurerm_private_dns_srv_record" "ldapdefault-first-site-name_sitesdc_m
 resource "azurerm_private_dns_srv_record" "kerberosdefault-first-site-name_sitesdc_msdcs" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_kerberos.default-first-site-name._sites.dc._msdcs"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
@@ -227,8 +229,8 @@ resource "azurerm_private_dns_srv_record" "kerberosdefault-first-site-name_sites
 resource "azurerm_private_dns_srv_record" "ldapdefault-first-site-name_sitesgc_msdcs" {
   count               = local.create_dns_records ? 1 : 0
   name                = "_ldap.default-first-site-name._sites.gc._msdcs"
-  resource_group_name = azurerm_private_dns_zone.azhop_private_dns.resource_group_name
-  zone_name           = azurerm_private_dns_zone.azhop_private_dns.name
+  resource_group_name = azurerm_private_dns_zone.azhop_private_dns[0].resource_group_name
+  zone_name           = azurerm_private_dns_zone.azhop_private_dns[0].name
   ttl                 = 3600
   dynamic "record" {
     for_each = local.domain_controlers
