1f535a8b8b7f8ad6a01ed73a3f92d7e631388555
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2023-11-08 09:50:59 +0100

    use azurehpc-health-checks

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/8-configure-nhc.sh
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/8-configure-nhc.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$SCRIPT_DIR/../files/azhop-helpers.sh" 
-read_os
 
-$SCRIPT_DIR/../files/$os_release/init_nhc.sh
-
-# Install custom checks
-cp -v $SCRIPT_DIR/../files/nhc/scripts/* /etc/nhc/scripts/
+# Install NHC if not already installed in the image
+if [ ! -f /usr/sbin/nhc ] || [ ! -d /opt/azurehpc/test/azurehpc-health-checks ] ; then
+    mkdir -p /opt/azurehpc/test/
+    cd /opt/azurehpc/test/
+    git clone https://github.com/Azure/azurehpc-health-checks.git
+    cd azurehpc-health-checks
+    ./install-nhc.sh
+fi
 
 # Install azhop-node-offline.sh
 cp -v $SCRIPT_DIR/../files/nhc/azhop-node-offline.sh /usr/libexec/nhc/
 chmod 755 /usr/libexec/nhc/azhop-node-offline.sh
 
+# Use our own NHC config files
 NHC_CONFIG_FILE="/etc/nhc/nhc.conf"
 VM_SIZE=$(curl -s --noproxy "*" -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2019-08-15" | jq -r '.vmSize' | tr '[:upper:]' '[:lower:]' | sed 's/standard_//')
 
