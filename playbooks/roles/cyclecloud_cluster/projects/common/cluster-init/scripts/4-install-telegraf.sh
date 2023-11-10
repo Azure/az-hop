92127ce31e510425088bd58300b8a2a839ea4c2a
Esthela Gallardo <egallardo@microsoft.com>
2023-11-10 03:17:40 -0600

    Feature/cluster dashboard (#1721)
    * adding automation to capture new information in influx
    
    * adding new monitoring dashboards
    
    * removing vmss as a key and updating dashboards
    
    * allowing setting the default thresholds for the grafana dashboards
    
    * updating dashboards to move networking panels into the compute servers dashboard
    
    * final changes to cluster stats dashboard

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/4-install-telegraf.sh
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/4-install-telegraf.sh
@@ -9,10 +9,11 @@ echo "Configuring global tags"
 AZHPC_VMSIZE=$(curl -s --noproxy "*" -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2019-08-15" | jq -r '.vmSize' | tr '[:upper:]' '[:lower:]')
 PHYSICAL_HOST=$(strings /var/lib/hyperv/.kvp_pool_3 | grep -A1 PhysicalHostName | head -n 2 | tail -1)
 VMSS=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2018-10-01" | jq -r '.compute.vmScaleSetName')
+ARRAY=$(jetpack config cyclecloud.node.template)
 
 sed -i "s/__SKU__/${AZHPC_VMSIZE}/g" ../files/telegraf.conf
 sed -i "s/__PHYS_HOST__/${PHYSICAL_HOST}/g" ../files/telegraf.conf
-sed -i "s/__VMSS__/${VMSS}/g" ../files/telegraf.conf
+sed -i "s/__ARRAY__/${ARRAY}/g" ../files/telegraf.conf
 
 echo "Copy configuration file to use"
 TELEGRAF_CONF_DIR=/etc/telegraf
