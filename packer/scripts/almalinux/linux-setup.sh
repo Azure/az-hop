2acdae881402aa2f5a4d67f7a5b88fb48275bdd1
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-02-19 10:36:41 +0100

    move apptainer in the images

--- packer/scripts/almalinux/linux-setup.sh
+++ packer/scripts/almalinux/linux-setup.sh
@@ -30,4 +30,7 @@ cd /usr/local/bin
 wget -q https://aka.ms/downloadazcopy-v10-linux -O - | tar zxf - --strip-components 1 --wildcards '*/azcopy'
 chmod 755 /usr/local/bin/azcopy
 
+# Install apptainer
+dnf install -y https://github.com/apptainer/apptainer/releases/download/v1.2.5/apptainer-1.2.5-1.x86_64.rpm
+
 dnf -y update --security
--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/files/ubuntu/init_apptainer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-add-apt-repository -y ppa:apptainer/ppa
-apt install -y apptainer
