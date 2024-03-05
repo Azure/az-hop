2ede56301bff3b01cf0f09954e8c0e01ff032e6e
Xavier Pillons <xpillons@microsoft.com>
2024-03-05 18:56:21 +0100

    Fix slurm mpipingpong job templates (#1874)
    * add missing I_MPI_xxx values for slurm
    
    * add exclusive

--- playbooks/files/ood_templates/slurm/impi_pingpong/pingpong.sh
+++ playbooks/files/ood_templates/slurm/impi_pingpong/pingpong.sh
@@ -5,6 +5,7 @@
 #SBATCH -p hpc
 #SBATCH -t 5
 #SBATCH --export=NONE
+#SBACTH --exclusive
 
 source /etc/profile.d/modules.sh
 module use /usr/share/Modules/modulefiles
@@ -13,7 +14,7 @@ module load mpi/impi-2021
 
 export I_MPI_DEBUG=5
 export I_MPI_HYDRA_BOOTSTRAP=slurm
-#export I_MPI_PORT_RANGE=59000:61000
+export I_MPI_PORT_RANGE=59000:61000
 
 echo "Running on $SLURM_NNODES nodes"
 mpirun -ppn 1 IMB-MPI1 PingPong
