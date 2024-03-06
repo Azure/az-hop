122c2220d4dfba884cd2e3c84ea14a2cf5b86bc8
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-03-06 16:10:33 +0100

    use aznhc 0.2.9

--- playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/8-configure-nhc.sh
+++ playbooks/roles/cyclecloud_cluster/projects/common/cluster-init/scripts/8-configure-nhc.sh
@@ -10,7 +10,7 @@ fi
 
 # Install NHC if not already installed in the image
 az_nhc_installed_version=$(grep Azure-NHC /opt/azurehpc/test/azurehpc-health-checks/docs/version.log | cut -d':' -f2 | xargs)
-az_nhc_target_version="v0.2.7"
+az_nhc_target_version="v0.2.9"
 
 if [ "$az_nhc_installed_version" != "$az_nhc_target_version" ] ; then
     if [ -d /opt/azurehpc/test/azurehpc-health-checks ]; then
@@ -18,46 +18,54 @@ if [ "$az_nhc_installed_version" != "$az_nhc_target_version" ] ; then
     fi
     mkdir -p /opt/azurehpc/test/
     cd /opt/azurehpc/test/
-    git clone https://github.com/Azure/azurehpc-health-checks.git -b v0.2.7
+    git clone https://github.com/Azure/azurehpc-health-checks.git -b $az_nhc_target_version
     cd azurehpc-health-checks
     ./install-nhc.sh
 fi
 
-# Install azhop-node-offline.sh
-cp -v $SCRIPT_DIR/../files/nhc/azhop-node-offline.sh /usr/libexec/nhc/
-chmod 755 /usr/libexec/nhc/azhop-node-offline.sh
-
-# Use our own NHC config files
-NHC_CONFIG_FILE="/etc/nhc/nhc.conf"
-VM_SIZE=$(curl -s --noproxy "*" -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2019-08-15" | jq -r '.vmSize' | tr '[:upper:]' '[:lower:]' | sed 's/standard_//')
-
-NHC_CONFIG_EXTRA="$SCRIPT_DIR/../files/nhc/nhc_${VM_SIZE}.conf"
-
-# Use common config for all compute nodes
-if [ -e $NHC_CONFIG_FILE ]; then
-    rm -f ${NHC_CONFIG_FILE}.bak
-    mv $NHC_CONFIG_FILE ${NHC_CONFIG_FILE}.bak
-fi
-cp -fv $SCRIPT_DIR/../files/nhc/nhc_common.conf $NHC_CONFIG_FILE
-
-# Append VM size specific config if exists
-if [ -e $NHC_CONFIG_EXTRA  ]; then
-    cat $NHC_CONFIG_EXTRA >> $NHC_CONFIG_FILE
-fi
-
-# Add nvidia-smi health checks for GPU SKUs except NV_v4 as they don't have NVIDIA device
-case $VM_SIZE in
-    nv*v4)
-    ;;
-    nc*|nv*|nd*)
-        echo " * || check_nvsmi_healthmon" >> $NHC_CONFIG_FILE
-    ;;
-    # Check HDR InfiniBand on all HBv2 and HBv3 SKUs
-    hb*v2|hb*v3)
-        echo " * || check_hw_ib 200 mlx5_ib0:1" >> $NHC_CONFIG_FILE
-    ;;
-    hc44rs)
-        echo " * || check_hw_ib 100 mlx5_ib0:1" >> $NHC_CONFIG_FILE
-    ;;
-
+. /etc/os-release
+case $ID in
+    ubuntu)
+        LIBEXEDIR=/usr/lib;;
+    *) 
+        LIBEXEDIR=/usr/libexec;;
 esac
+
+# Install azhop-node-offline.sh
+cp -v $SCRIPT_DIR/../files/nhc/azhop-node-offline.sh $LIBEXEDIR/nhc/
+chmod 755 $LIBEXEDIR/nhc/azhop-node-offline.sh
+
+# # Use our own NHC config files
+# NHC_CONFIG_FILE="/etc/nhc/nhc.conf"
+# VM_SIZE=$(curl -s --noproxy "*" -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2019-08-15" | jq -r '.vmSize' | tr '[:upper:]' '[:lower:]' | sed 's/standard_//')
+
+# NHC_CONFIG_EXTRA="$SCRIPT_DIR/../files/nhc/nhc_${VM_SIZE}.conf"
+
+# # Use common config for all compute nodes
+# if [ -e $NHC_CONFIG_FILE ]; then
+#     rm -f ${NHC_CONFIG_FILE}.bak
+#     mv $NHC_CONFIG_FILE ${NHC_CONFIG_FILE}.bak
+# fi
+# cp -fv $SCRIPT_DIR/../files/nhc/nhc_common.conf $NHC_CONFIG_FILE
+
+# # Append VM size specific config if exists
+# if [ -e $NHC_CONFIG_EXTRA  ]; then
+#     cat $NHC_CONFIG_EXTRA >> $NHC_CONFIG_FILE
+# fi
+
+# # Add nvidia-smi health checks for GPU SKUs except NV_v4 as they don't have NVIDIA device
+# case $VM_SIZE in
+#     nv*v4)
+#     ;;
+#     nc*|nv*|nd*)
+#         echo " * || check_nvsmi_healthmon" >> $NHC_CONFIG_FILE
+#     ;;
+#     # Check HDR InfiniBand on all HBv2 and HBv3 SKUs
+#     hb*v2|hb*v3)
+#         echo " * || check_hw_ib 200 mlx5_ib0:1" >> $NHC_CONFIG_FILE
+#     ;;
+#     hc44rs)
+#         echo " * || check_hw_ib 100 mlx5_ib0:1" >> $NHC_CONFIG_FILE
+#     ;;
+
+# esac
