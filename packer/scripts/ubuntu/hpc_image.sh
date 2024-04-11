f6bb93bcede1f4054ec5c6fbb974bf242afdbc68
egmsft <egallardo@microsoft.com>
2024-04-11 09:28:50 -0500

    adding ubuntu desktop image and cleaning up scripts

--- packer/scripts/ubuntu/hpc_image.sh
+++ packer/scripts/ubuntu/hpc_image.sh
@@ -11,6 +11,8 @@ cd /mnt/
 
 git clone https://github.com/Azure/azhpc-images -b ubuntu-hpc-20231127
 
+sed -i 's/LUSTRE_VERSION=2.15.1-29-gbae0abe/LUSTRE_VERSION=2.15.4-42-gd6d405d/g' ./azhpc-images/ubuntu/common/install_lustre_client.sh
+
 cd ./azhpc-images/ubuntu/ubuntu-20.x/ubuntu-20.04-hpc
 
 ./install.sh
